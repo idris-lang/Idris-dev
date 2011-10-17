2c4ff83b993e675d668f316d466e13b5d408a87d
Edwin Brady <eb@cs.st-andrews.ac.uk>
2011-10-17 21:41:42 +0100

    Change unification in function applications

--- src/Core/Unify.hs
+++ src/Core/Unify.hs
@@ -10,88 +10,69 @@ import Debug.Trace
 -- which can be filled in, by matching one term's normal form against another.
 -- Returns a list of hole names paired with the term which solves them.
 
--- FIXME! Needs to be cleverer: when we solve a variable, fill it in and normalise
--- to help the rest of the unification
-
 unify :: Context -> Env -> TT Name -> TT Name -> TC [(Name, TT Name)]
 unify ctxt env topx topy 
-    = case un' [] (normalise ctxt env topx) (normalise ctxt env topy) of
+    = case un' False [] (normalise ctxt env topx) (normalise ctxt env topy) of
               OK v -> return v
               Error e -> tfail $ CantUnify topx topy e  
   where
-    un' bnames (P Bound x _)  (P Bound y _)  
+    injective (P (DCon _ _) _ _) = True
+    injective (P (TCon _) _ _)   = True
+    injective (App f a)          = injective f
+    injective _                  = False
+
+    un' fn bnames (P Bound x _)  (P Bound y _)  
         | (x,y) `elem` bnames = return []
-    un' bnames (P Bound x _) tm
-        | holeIn env x = return [(x, tm)]
-    un' bnames tm (P Bound y _)
-        | holeIn env y = return [(y, tm)]
-    un' bnames (V i) (P Bound x _)
+    un' fn bnames (P Bound x _) tm
+        | (not fn || injective tm) 
+          && holeIn env x = return [(x, tm)]
+    un' fn bnames tm (P Bound y _)
+        | (not fn || injective tm)
+          && holeIn env y = return [(y, tm)]
+    un' fn bnames (V i) (P Bound x _)
         | fst (bnames!!i) == x || snd (bnames!!i) == x = return []
-    un' bnames (P Bound x _) (V i)
+    un' fn bnames (P Bound x _) (V i)
         | fst (bnames!!i) == x || snd (bnames!!i) == x = return []
 
-    un' bnames x@(App _ _)    y@(App _ _)    
-        = uApp bnames (unApply x) (unApply y)
-    un' bnames x (Bind n (Lam t) (App y (P Bound n' _)))
-        | n == n' = un' bnames x y
-    un' bnames (Bind n (Lam t) (App x (P Bound n' _))) y
-        | n == n' = un' bnames x y
-    un' bnames (Bind x bx sx) (Bind y by sy) 
+    un' fn bnames (App fx ax) (App fy ay)    
+        = do hf <- un' True bnames fx fy 
+             let ax' = normalise ctxt env (substNames hf ax)
+             let ay' = normalise ctxt env (substNames hf ay)
+             ha <- un' False bnames ax' ay'
+             combine bnames hf ha
+
+    un' fn bnames x (Bind n (Lam t) (App y (P Bound n' _)))
+        | n == n' = un' False bnames x y
+    un' fn bnames (Bind n (Lam t) (App x (P Bound n' _))) y
+        | n == n' = un' False bnames x y
+    un' fn bnames (Bind x bx sx) (Bind y by sy) 
         = do h1 <- uB bnames bx by
-             h2 <- un' ((x,y):bnames) sx sy
+             h2 <- un' False ((x,y):bnames) sx sy
              combine bnames h1 h2
-    un' bnames x y | x == y = return []
-                   | otherwise = fail $ "Can't unify " ++ show x ++ " and " ++ show y
-                                        ++ " " ++ show bnames ++ " " ++ show env
+    un' fn bnames x y 
+        | x == y = return []
+        | otherwise = fail $ "Can't unify " ++ show x ++ " and " ++ show y
+                             ++ " " ++ show bnames ++ " " ++ show env
 
     uB bnames (Let tx vx) (Let ty vy)
-        = do h1 <- un' bnames tx ty
-             h2 <- un' bnames ty vy
+        = do h1 <- un' False bnames tx ty
+             h2 <- un' False bnames ty vy
              combine bnames h1 h2
     uB bnames (Guess tx vx) (Guess ty vy)
-        = do h1 <- un' bnames tx ty
-             h2 <- un' bnames ty vy
+        = do h1 <- un' False bnames tx ty
+             h2 <- un' False bnames ty vy
              combine bnames h1 h2
-    uB bnames (Lam tx) (Lam ty) = un' bnames tx ty
-    uB bnames (Pi tx) (Pi ty) = un' bnames tx ty
-    uB bnames (Hole tx) (Hole ty) = un' bnames tx ty
-    uB bnames (PVar tx) (PVar ty) = un' bnames tx ty
+    uB bnames (Lam tx) (Lam ty) = un' False bnames tx ty
+    uB bnames (Pi tx) (Pi ty) = un' False bnames tx ty
+    uB bnames (Hole tx) (Hole ty) = un' False bnames tx ty
+    uB bnames (PVar tx) (PVar ty) = un' False bnames tx ty
     uB bnames _ _ = fail "Can't unify"
-
-    uApp bnames (xp@(P xnt x xty), xargs) (yp@(P ynt y yty), yargs)
-        | (x == y && all okToUnify [xnt, ynt]) || -- functions x,y are injectice
-          (holeIn env x && okToUnify ynt) ||
-          (okToUnify xnt && holeIn env y)
-            = do h <- un' bnames xp yp
-                 uArgs bnames h xargs yargs
-        | x == y && xargs == yargs -- only ok if arguments are equal, if functions
-                                   -- are not known to be injetive
-            = return []
-    uApp bnames (xf, xargs) (yf, yargs) 
-            = do un' bnames xf yf -- ignore result
-                 uArgs bnames [] xargs yargs
-
-    okToUnify (DCon _ _) = True
-    okToUnify (TCon _) = True
-    okToUnify _ = False
-
-    uArgs bnames h [] [] = return h
-    uArgs bnames h (x:xs) (y:ys) = 
-        case un' bnames x y of
-           OK h' -> do next <- combine bnames h h' 
-                       uArgs bnames next xs ys
-           _ -> do let x' = normalise ctxt env (substNames h x)
-                   let y' = normalise ctxt env (substNames h y)
-                   h' <- un' bnames x' y'
-                   next <- combine bnames h h'
-                   uArgs bnames next xs ys
-    uArgs bnames h _ _ = fail "Can't unify; argument lists different length"
 
     combine bnames as [] = return as
     combine bnames as ((n, t) : bs)
         = case lookup n as of 
             Nothing -> combine bnames (as ++ [(n,t)]) bs
-            Just t' -> do un' bnames t t'
+            Just t' -> do un' False bnames t t'
                           combine bnames as bs
 
 holeIn :: Env -> Name -> Bool
