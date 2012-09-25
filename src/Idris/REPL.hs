28325170b326bd37724c3d24feb27c099af3666c
Edwin Brady <eb@cs.st-andrews.ac.uk>
2012-09-25 11:15:35 +0100

    Move main setup code to Idris.REPL

--- src/Idris/REPL.hs
+++ src/Idris/REPL.hs
@@ -12,6 +12,7 @@ import Idris.Delaborate
 import Idris.Compiler
 import Idris.Prover
 import Idris.Parser
+import Idris.Primitives
 import Idris.Coverage
 import Paths_idris
 import Util.System
@@ -22,6 +23,7 @@ import Core.TT
 import Core.Constraints
 
 import IRTS.Compiler
+import IRTS.LParser
 
 -- import RTS.SC
 -- import RTS.Bytecode
@@ -30,12 +32,14 @@ import IRTS.Compiler
 
 import System.Console.Haskeline as H
 import System.FilePath
+import System.Exit
 import System.Environment
 import System.Process
 import System.Directory
 import System.IO
 import Control.Monad
 import Control.Monad.State
+import Data.Maybe
 import Data.List
 import Data.Char
 import Data.Version
@@ -383,4 +387,98 @@ help =
     ([":unset"], "<option>", "Unset an option"),
     ([":q",":quit"], "", "Exit the Idris system")
   ]
+
+-- invoke as if from command line
+idris :: [Opt] -> IO IState
+idris opts = runInputT defaultSettings $ execStateT (idrisMain opts) idrisInit
+
+idrisMain :: [Opt] -> Idris ()
+idrisMain opts = 
+    do let inputs = opt getFile opts
+       let runrepl = not (NoREPL `elem` opts)
+       let output = opt getOutput opts
+       let newoutput = opt getNewOutput opts
+       let ibcsubdir = opt getIBCSubDir opts
+       let importdirs = opt getImportDir opts
+       let bcs = opt getBC opts
+       let vm = opt getFOVM opts
+       setREPL runrepl
+       setVerbose runrepl
+       when (Verbose `elem` opts) $ setVerbose True
+       mapM_ makeOption opts
+       -- if we have the --fovm flag, drop into the first order VM testing
+       case vm of
+	    [] -> return ()
+	    xs -> liftIO $ mapM_ fovm xs 
+       -- if we have the --bytecode flag, drop into the bytecode assembler
+       case bcs of
+	    [] -> return ()
+	    xs -> return () -- liftIO $ mapM_ bcAsm xs 
+       case ibcsubdir of
+         [] -> setIBCSubDir ""
+         (d:_) -> setIBCSubDir d
+       setImportDirs importdirs
+       elabPrims
+       when (not (NoPrelude `elem` opts)) $ do x <- loadModule "prelude"
+                                               return ()
+       when runrepl $ iputStrLn banner 
+       ist <- get
+       mods <- mapM loadModule inputs
+       ok <- noErrors
+       when ok $ case output of
+                    [] -> return ()
+                    (o:_) -> process "" (Compile ViaC o)  
+       when ok $ case newoutput of
+                    [] -> return ()
+                    (o:_) -> process "" (NewCompile o)  
+       when runrepl $ repl ist inputs
+       ok <- noErrors
+       when (not ok) $ liftIO (exitWith (ExitFailure 1))
+  where
+    makeOption (OLogging i) = setLogLevel i
+    makeOption TypeCase = setTypeCase True
+    makeOption TypeInType = setTypeInType True
+    makeOption NoCoverage = setCoverage False
+    makeOption ErrContext = setErrContext True
+    makeOption _ = return ()
+
+getFile :: Opt -> Maybe String
+getFile (Filename str) = Just str
+getFile _ = Nothing
+
+getBC :: Opt -> Maybe String
+getBC (BCAsm str) = Just str
+getBC _ = Nothing
+
+getFOVM :: Opt -> Maybe String
+getFOVM (FOVM str) = Just str
+getFOVM _ = Nothing
+
+getOutput :: Opt -> Maybe String
+getOutput (Output str) = Just str
+getOutput _ = Nothing
+
+getNewOutput :: Opt -> Maybe String
+getNewOutput (NewOutput str) = Just str
+getNewOutput _ = Nothing
+
+getIBCSubDir :: Opt -> Maybe String
+getIBCSubDir (IBCSubDir str) = Just str
+getIBCSubDir _ = Nothing
+
+getImportDir :: Opt -> Maybe String
+getImportDir (ImportDir str) = Just str
+getImportDir _ = Nothing
+
+opt :: (Opt -> Maybe a) -> [Opt] -> [a]
+opt = mapMaybe 
+
+ver = showVersion version
+
+banner = "     ____    __     _                                          \n" ++     
+         "    /  _/___/ /____(_)____                                     \n" ++
+         "    / // __  / ___/ / ___/     Version " ++ ver ++ "\n" ++
+         "  _/ // /_/ / /  / (__  )      http://www.idris-lang.org/      \n" ++
+         " /___/\\__,_/_/  /_/____/       Type :? for help                \n" 
+
 
--- src/Main.hs
+++ src/Main.hs
@@ -25,9 +25,6 @@ import Idris.Primitives
 import Idris.Imports
 import Idris.Error
 
-import IRTS.Lang
-import IRTS.LParser
-
 import Paths_idris
 
 -- Main program reads command line options, parses the main program, and gets
@@ -38,91 +35,13 @@ main = do xs <- getArgs
           runInputT defaultSettings $ execStateT (runIdris opts) idrisInit
 
 runIdris :: [Opt] -> Idris ()
-runIdris opts = 
-    do let inputs = opt getFile opts
-       let runrepl = not (NoREPL `elem` opts)
-       let output = opt getOutput opts
-       let newoutput = opt getNewOutput opts
-       let ibcsubdir = opt getIBCSubDir opts
-       let importdirs = opt getImportDir opts
-       let bcs = opt getBC opts
-       let vm = opt getFOVM opts
-
+runIdris opts = do
        when (Ver `elem` opts) $ liftIO showver
        when (Usage `elem` opts) $ liftIO usage
        when (ShowIncs `elem` opts) $ liftIO showIncs
        when (ShowLibs `elem` opts) $ liftIO showLibs
        when (ShowLibdir `elem` opts) $ liftIO showLibdir
-       setREPL runrepl
-       setVerbose runrepl
-       when (Verbose `elem` opts) $ setVerbose True
-       mapM_ makeOption opts
-       -- if we have the --fovm flag, drop into the first order VM testing
-       case vm of
-	    [] -> return ()
-	    xs -> liftIO $ mapM_ fovm xs 
-       -- if we have the --bytecode flag, drop into the bytecode assembler
-       case bcs of
-	    [] -> return ()
-	    xs -> return () -- liftIO $ mapM_ bcAsm xs 
-       case ibcsubdir of
-         [] -> setIBCSubDir ""
-         (d:_) -> setIBCSubDir d
-       setImportDirs importdirs
-       elabPrims
-       when (not (NoPrelude `elem` opts)) $ do x <- loadModule "prelude"
-                                               return ()
-       when runrepl $ iputStrLn banner 
-       ist <- get
-       mods <- mapM loadModule inputs
-       ok <- noErrors
-       when ok $ case output of
-                    [] -> return ()
-                    (o:_) -> process "" (Compile ViaC o)  
-       when ok $ case newoutput of
-                    [] -> return ()
-                    (o:_) -> process "" (NewCompile o)  
-       when runrepl $ repl ist inputs
-       ok <- noErrors
-       when (not ok) $ liftIO (exitWith (ExitFailure 1))
-  where
-    makeOption (OLogging i) = setLogLevel i
-    makeOption TypeCase = setTypeCase True
-    makeOption TypeInType = setTypeInType True
-    makeOption NoCoverage = setCoverage False
-    makeOption ErrContext = setErrContext True
-    makeOption _ = return ()
-
-getFile :: Opt -> Maybe String
-getFile (Filename str) = Just str
-getFile _ = Nothing
-
-getBC :: Opt -> Maybe String
-getBC (BCAsm str) = Just str
-getBC _ = Nothing
-
-getFOVM :: Opt -> Maybe String
-getFOVM (FOVM str) = Just str
-getFOVM _ = Nothing
-
-getOutput :: Opt -> Maybe String
-getOutput (Output str) = Just str
-getOutput _ = Nothing
-
-getNewOutput :: Opt -> Maybe String
-getNewOutput (NewOutput str) = Just str
-getNewOutput _ = Nothing
-
-getIBCSubDir :: Opt -> Maybe String
-getIBCSubDir (IBCSubDir str) = Just str
-getIBCSubDir _ = Nothing
-
-getImportDir :: Opt -> Maybe String
-getImportDir (ImportDir str) = Just str
-getImportDir _ = Nothing
-
-opt :: (Opt -> Maybe a) -> [Opt] -> [a]
-opt = mapMaybe 
+       idrisMain opts -- in Idris.REPL
 
 usage = do putStrLn usagemsg
            exitWith ExitSuccess
@@ -164,14 +83,6 @@ parseArgs ("-i":n:ns)           = liftM (ImportDir n : ) (parseArgs ns)
 parseArgs ("--bytecode":n:ns)   = liftM (\x -> NoREPL : BCAsm n : x) (parseArgs ns)
 parseArgs ("--fovm":n:ns)       = liftM (\x -> NoREPL : FOVM n : x) (parseArgs ns)
 parseArgs (n:ns)                = liftM (Filename n : ) (parseArgs ns)
-
-ver = showVersion version
-
-banner = "     ____    __     _                                          \n" ++     
-         "    /  _/___/ /____(_)____                                     \n" ++
-         "    / // __  / ___/ / ___/     Version " ++ ver ++ "\n" ++
-         "  _/ // /_/ / /  / (__  )      http://www.idris-lang.org/      \n" ++
-         " /___/\\__,_/_/  /_/____/       Type :? for help                \n" 
 
 usagemsg = "Idris version " ++ ver ++ "\n" ++
            "--------------" ++ map (\x -> '-') ver ++ "\n" ++
