aa0693e638141446ad6ff2ee68da07afda5d4a60
Edwin Brady <ecb10@st-andrews.ac.uk>
2012-11-13 11:22:26 +0000

    Disambiguate catch explicitly in CodegenCommon

--- src/IRTS/CodegenCommon.hs
+++ src/IRTS/CodegenCommon.hs
@@ -12,11 +12,11 @@ data DbgLevel = NONE | DEBUG | TRACE deriving Eq
 data OutputType = Raw | Object | Executable deriving (Eq, Show)
 
 environment :: String -> IO (Maybe String)
-environment x = catch (do e <- getEnv x
-                          return (Just e))
-#if MIN_VERSION_base(4,6,0)
+environment x = Control.Exception.catch (do e <- getEnv x
+                                            return (Just e))
+#if MIN_VERSION_base(4,0,0)
                           (\y-> do return (y::SomeException);  return Nothing)
 #endif
-#if !MIN_VERSION_base(4,6,0)
+#if !MIN_VERSION_base(4,0,0)
                           (\_->  return Nothing)
 #endif
