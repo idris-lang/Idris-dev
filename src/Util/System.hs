1d0c6aa736b1063982d73dcba2d168b28e994c93
Chetan T <chetant@gmail.com>
2012-11-27 07:47:40 -0500

    Fixing compilation in Mingw32

--- src/Util/System.hs
+++ src/Util/System.hs
@@ -1,13 +1,19 @@
 {-# LANGUAGE CPP #-}
-module Util.System(tempfile,environment,getCC) where
+module Util.System(tempfile,environment,getCC,getLibFlags,getIdrisLibDir,getIncFlags) where
 
 -- System helper functions.
 
+import System.FilePath ((</>), addTrailingPathSeparator, normalise)
 import System.Environment
 import System.IO
 #if MIN_VERSION_base(4,0,0)
 import Control.Exception as CE
 #endif
+
+-- import Data.List
+import Data.List.Split(splitOn)
+
+import Paths_idris
 
 #if MIN_VERSION_base(4,0,0)
 catchIO :: IO a -> (IOError -> IO a) -> IO a
@@ -23,13 +29,32 @@ getCC = do env <- environment "IDRIS_CC"
                 Just cc -> return cc
 
 tempfile :: IO (FilePath, Handle)
-tempfile = do env <- environment "TMPDIR"
+tempfile = do env <- environment tempDirEnvVar
               let dir = case env of
                               Nothing -> "/tmp"
+#ifdef mingw32_HOST_OS
+                              -- ghc changes the path separators if we start program in a *nix shell
+                              (Just d) -> normalise d
+#else
                               (Just d) -> d
+#endif
               openTempFile dir "idris"
+#ifdef mingw32_HOST_OS
+    where tempDirEnvVar = "TMP"
+#else
+    where tempDirEnvVar = "TMPDIR"
+#endif
 
 environment :: String -> IO (Maybe String)
 environment x = catchIO (do e <- getEnv x
                             return (Just e))
                       (\_ -> return Nothing)
+
+getLibFlags = do dir <- getDataDir
+                 return $ "-L" ++ (dir </> "rts") ++ " -lidris_rts -lgmp -lpthread"
+
+getIdrisLibDir = do dir <- getDataDir
+                    return $ addTrailingPathSeparator dir
+
+getIncFlags = do dir <- getDataDir
+                 return $ "-I" ++ dir </> "rts"
