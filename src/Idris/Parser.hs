b2cf96765d75109b6613c256f633426fbbe63db8
Edwin Brady <ecb10@st-andrews.ac.uk>
2013-01-07 11:58:45 +0000

    Allow class constraints in syntax definitions

--- src/Idris/Parser.hs
+++ src/Idris/Parser.hs
@@ -390,7 +390,7 @@ pSyntaxRule syn
          when (length ns /= length (nub ns)) 
             $ fail "Repeated variable in syntax rule"
          lchar '='
-         tm <- pExpr (impOK syn)
+         tm <- pTExpr (impOK syn)
          pTerminator
          return (Rule (mkSimple syms) tm sty)
   where
@@ -908,10 +908,11 @@ mkType (NS n s) = NS (mkType n) s
 noImp syn = syn { implicitAllowed = False }
 impOK syn = syn { implicitAllowed = True }
 
-pTSig syn = do lchar ':'
-               cs <- if implicitAllowed syn then pConstList syn else return []
-               sc <- pExpr syn 
-               return (bindList (PPi constraint) (map (\x -> (MN 0 "c", x)) cs) sc)
+pTSig syn = do lchar ':'; pTExpr syn
+
+pTExpr syn = do cs <- if implicitAllowed syn then pConstList syn else return []
+                sc <- pExpr syn 
+                return (bindList (PPi constraint) (map (\x -> (MN 0 "c", x)) cs) sc)
 
 pLambda syn = do lchar '\\'
                  try (do xt <- tyOptDeclList syn
