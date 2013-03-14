f5e4128e4abab121e26767ec1d2d7dbf13eb92f6
David Christiansen <david@davidchristiansen.dk>
2013-03-14 10:24:12 +0100

    Start of FFI infrastructure
    Now I can convert Idris' FFI data to similar Haskell types.

--- src/Core/Execute.hs
+++ src/Core/Execute.hs
@@ -8,6 +8,10 @@ import Core.Evaluate
 
 import Debug.Trace
 
+import Foreign.LibFFI
+import Foreign.Ptr (nullPtr)
+import System.Posix.DynamicLinker
+
 -- | Attempt to perform a side effect. Return either Just the next step in
 -- evaluation (after performing the side effect through IO), or Nothing if no
 -- IO was performed.
@@ -26,7 +30,45 @@ execute tm = do stepped <- step tm
                   Nothing -> return tm
                   Just tm' -> execute tm'
 
+data FTy = FInt | FFloat | FChar | FString | FPtr | FUnit deriving Show
+
+data Foreign = FFun String [FTy] FTy deriving Show
+
+call :: Foreign -> [TT Name] -> Idris (TT Name)
+call (FFun name argTypes retType) args = undefined
+
+foreignFromTT :: TT Name -> Maybe Foreign
+foreignFromTT t = case (unApply t) of
+                    (_, [(Constant (Str name)), args, ret]) ->
+                        do trace (show $ unList args) $ return ()
+                           argTy <- unList args
+                           argFTy <- sequence $ map getFTy argTy
+                           retFTy <- getFTy ret
+                           return $ FFun name argFTy retFTy
+                    _ -> Nothing
+
+getFTy :: TT Name -> Maybe FTy
+getFTy (P _ (UN t) _) = case t of
+                          "FInt" -> Just FInt
+                          "FFloat" -> Just FFloat
+                          "FChar" -> Just FChar
+                          "FString" -> Just FString
+                          "FPtr" -> Just FPtr
+                          "FUnit" -> Just FUnit
+                          _ -> Nothing
+getFTy _ = Nothing
+
+unList :: TT Name -> Maybe [TT Name]
+unList tm = case unApply tm of
+              (nil, [_]) -> Just []
+              (cons, ([_, x, xs])) ->
+                  do rest <- unList xs
+                     return $ x:rest
+              (f, args) -> Nothing
 
 stepForeign :: [TT Name] -> Idris (TT Name)
-stepForeign args = do iputStrLn $ "FOREIGN CALL" ++ show args
-                      return $ mkApp (P Bound (UN "mkForeign") Erased) args
+stepForeign (ty:fn:args) = do iputStrLn $ "FOREIGN CALL " ++ show (ty:fn:args)
+                              iputStrLn $ show $ foreignFromTT fn
+                              iputStrLn ""
+                              return $ mkApp (P Bound (UN "mkForeign") Erased) args
+stepForeign _ = fail "Tried to call foreign function that wasn't mkForeign"
