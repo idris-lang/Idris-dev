90f661beded96d725a8a315afd7577f02444f62e
David Christiansen <david@davidchristiansen.dk>
2013-03-22 10:07:48 +0100

    Make dynamic library extension platform-dependent
    New syntax is %dynamic "libm" instead of %dynamic "libm.so".

--- src/Util/DynamicLinker.hs
+++ src/Util/DynamicLinker.hs
@@ -1,18 +1,27 @@
 -- | Platform-specific dynamic linking support. Add new platforms to this file
 -- through conditional compilation.
-{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE ExistentialQuantification, CPP #-}
 module Util.DynamicLinker where
 
 import Foreign.LibFFI
 import Foreign.Ptr (nullPtr, FunPtr, nullFunPtr)
 import System.Posix.DynamicLinker
 
+hostDynamicLibExt :: String
+#ifdef LINUX
+hostDynamicLibExt = "so"
+#elif MACOSX
+hostDynamicLibExt = "dylib"
+#elif WINDOWS
+hostDynamicLibExt = "dll"
+#endif
+
 data DynamicLib = Lib { lib_name :: String
                       , lib_handle :: DL
                       }
 
 tryLoadLib :: String -> IO (Maybe DynamicLib)
-tryLoadLib lib = do handle <- dlopen lib [RTLD_NOW]
+tryLoadLib lib = do handle <- dlopen (lib ++ "." ++ hostDynamicLibExt) [RTLD_NOW]
                     if undl handle == nullPtr
                       then return Nothing
                       else return . Just $ Lib lib handle
