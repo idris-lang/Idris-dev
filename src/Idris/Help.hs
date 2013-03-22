a029bd86df41f0927bc9b500ba74c7cd7468f923
Hannes Mehnert <hannes@mehnert.org>
2013-03-22 09:55:00 +0100

    reduce duplication by providing a show instance for CmdArg

--- src/Idris/Help.hs
+++ src/Idris/Help.hs
@@ -6,34 +6,44 @@ data CmdArg = ExprArg -- ^ The command takes an expression
             | ModuleArg -- ^ The command takes a module name
             | OptionArg -- ^ The command takes an option
             | MetaVarArg -- ^ The command takes a metavariable
-            | NoArg -- ^ No completion yet
+            | NoArg -- ^ No completion (yet!?)
+            | SpecialHeaderArg -- ^ do not use
 
--- TODO: Merge the second and fourth elements of the following tuples
-help :: [([String], String, String, CmdArg)]
+instance Show CmdArg where
+    show ExprArg = "<expr>"
+    show NameArg = "<name>"
+    show FileArg = "<filename>"
+    show ModuleArg = "<module>"
+    show OptionArg = "<option>"
+    show MetaVarArg = "<metavar>"
+    show NoArg = ""
+    show SpecialHeaderArg = "Arguments"
+
+help :: [([String], CmdArg, String)]
 help =
-  [ (["<expr>"], "", "Evaluate an expression", NoArg),
-    ([":t"], "<expr>", "Check the type of an expression", ExprArg),
-    ([":miss", ":missing"], "<name>", "Show missing clauses", NameArg),
-    ([":i", ":info"], "<name>", "Display information about a type class", NameArg),
-    ([":total"], "<name>", "Check the totality of a name", NameArg),
-    ([":r",":reload"], "", "Reload current file", NoArg),
-    ([":l",":load"], "<filename>", "Load a new file", FileArg),
-    ([":m",":module"], "<module>", "Import an extra module", ModuleArg), -- NOTE: dragons
-    ([":e",":edit"], "", "Edit current file using $EDITOR or $VISUAL", NoArg),
-    ([":m",":metavars"], "", "Show remaining proof obligations (metavariables)", MetaVarArg),
-    ([":p",":prove"], "<name>", "Prove a metavariable", MetaVarArg),
-    ([":a",":addproof"], "<name>", "Add proof to source file", NameArg),
-    ([":rmproof"], "<name>", "Remove proof from proof stack", NameArg),
-    ([":showproof"], "<name>", "Show proof", NameArg),
-    ([":proofs"], "", "Show available proofs", NoArg),
-    ([":x"], "<expr>", "Execute IO actions resulting from an expression using the interpreter", ExprArg),
-    ([":c",":compile"], "<filename>", "Compile to an executable <filename>", FileArg),
-    ([":js", ":javascript"], "<filename>", "Compile to JavaScript <filename>", FileArg),
-    ([":exec",":execute"], "", "Compile to an executable and run", NoArg),
-    ([":dynamic"], "<filename>", "Dynamically load a C library (similar to %dynamic)", FileArg),
-    ([":dynamic"], "", "List dynamically loaded C libraries", NoArg),
-    ([":?",":h",":help"], "", "Display this help text", NoArg),
-    ([":set"], "<option>", "Set an option (errorcontext, showimplicits)", OptionArg),
-    ([":unset"], "<option>", "Unset an option", OptionArg),
-    ([":q",":quit"], "", "Exit the Idris system", NoArg)
+  [ (["<expr>"], NoArg, "Evaluate an expression"),
+    ([":t"], ExprArg, "Check the type of an expression"),
+    ([":miss", ":missing"], NameArg, "Show missing clauses"),
+    ([":i", ":info"], NameArg, "Display information about a type class"),
+    ([":total"], NameArg, "Check the totality of a name"),
+    ([":r",":reload"], NoArg, "Reload current file"),
+    ([":l",":load"], FileArg, "Load a new file"),
+    ([":m",":module"], ModuleArg, "Import an extra module"), -- NOTE: dragons
+    ([":e",":edit"], NoArg, "Edit current file using $EDITOR or $VISUAL"),
+    ([":m",":metavars"], MetaVarArg, "Show remaining proof obligations (metavariables)"),
+    ([":p",":prove"], MetaVarArg, "Prove a metavariable"),
+    ([":a",":addproof"], NameArg, "Add proof to source file"),
+    ([":rmproof"], NameArg, "Remove proof from proof stack"),
+    ([":showproof"], NameArg, "Show proof"),
+    ([":proofs"], NoArg, "Show available proofs"),
+    ([":x"], ExprArg, "Execute IO actions resulting from an expression using the interpreter"),
+    ([":c",":compile"], FileArg, "Compile to an executable <filename>"),
+    ([":js", ":javascript"], FileArg, "Compile to JavaScript <filename>"),
+    ([":exec",":execute"], NoArg, "Compile to an executable and run"),
+    ([":dynamic"], FileArg, "Dynamically load a C library (similar to %dynamic)"),
+    ([":dynamic"], NoArg, "List dynamically loaded C libraries"),
+    ([":?",":h",":help"], NoArg, "Display this help text"),
+    ([":set"], OptionArg, "Set an option (errorcontext, showimplicits)"),
+    ([":unset"], OptionArg, "Unset an option"),
+    ([":q",":quit"], NoArg, "Exit the Idris system")
   ]
