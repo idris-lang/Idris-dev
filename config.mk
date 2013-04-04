1f2e71481591c93b03428be0924cb3c4246afa21
David Christiansen <david@davidchristiansen.dk>
2013-04-04 13:53:32 +0200

    OS detection in config.mk without using uname
    GCC has an option -dumpmachine which provides the necessary info.

--- config.mk
+++ config.mk
@@ -1,9 +1,31 @@
 GMP_INCLUDE_DIR :=
 CC              :=gcc
-SHLIB_SUFFIX    :=.so
-# Mac OS X: SHLIB_SUFFIX    :=.dylib
-# Windows: SHLIB_SUFFIX    :=.DLL
 CABAL           :=cabal
 CABALFLAGS	:=
 ## Enable Java RTS:
 #CABALFLAGS    :=-f Java
+
+MACHINE         := $(shell $(CC) -dumpmachine)
+ifneq (, $(findstring darwin, $(MACHINE)))
+	OS      :=darwin
+else
+ifneq (, $(findstring cygwin, $(MACHINE)))
+	OS      :=windows
+else
+ifneq (, $(findstring mingw, $(MACHINE)))
+	OS      :=windows
+else
+	OS      :=unix
+endif
+endif
+endif
+
+ifeq ($(OS),darwin)
+	SHLIB_SUFFIX    :=.dylib
+else
+ifeq ($(OS),windows)
+	SHLIB_SUFFIX    :=.DLL
+else
+	SHLIB_SUFFIX    :=.so
+endif
+endif
