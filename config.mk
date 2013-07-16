c5d58844fa6b783133e1e2f0c9562d683d6eae47
Jan Bessai <Jan.Bessai@tu-dortmund.de>
2013-07-16 19:53:14 +0200

    Remove Java RTS build in favor of the version hosted on Maven Central

--- config.mk
+++ config.mk
@@ -2,8 +2,6 @@ GMP_INCLUDE_DIR :=
 CC              :=gcc
 CABAL           :=cabal
 #CABALFLAGS	:=
-## Enable Java RTS:
-#CABALFLAGS    :=-f Java
 ## Disable building of Effects
 #CABALFLAGS :=-f NoEffects
 
