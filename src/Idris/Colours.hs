1beb71ca27f213310ce62eb62a9c452f82aa29ca
David Christiansen <david@davidchristiansen.dk>
2013-09-12 16:42:06 +0200

    Basic colour theme infrastructure

--- src/Idris/Colours.hs
+++ src/Idris/Colours.hs
@@ -2,31 +2,53 @@ module Idris.Colours where
 
 import System.Console.ANSI
 
+data IdrisColour = IdrisColour { colour    :: Color
+                               , vivid     :: Bool
+                               , underline :: Bool
+                               , bold      :: Bool
+                               }
+
+mkColour :: Color -> IdrisColour
+mkColour c = IdrisColour c True False False
+
+data ColourTheme = ColourTheme { keywordColour  :: IdrisColour
+                               , boundVarColour :: IdrisColour
+                               , implicitColour :: IdrisColour
+                               , functionColour :: IdrisColour
+                               , typeColour     :: IdrisColour
+                               , dataColour     :: IdrisColour
+                               }
+
+defaultTheme :: ColourTheme
+defaultTheme = ColourTheme { keywordColour = IdrisColour Black True True True
+                           , boundVarColour = mkColour Magenta
+                           , implicitColour = IdrisColour Magenta True True False
+                           , functionColour = mkColour Green
+                           , typeColour = mkColour Blue
+                           , dataColour = mkColour Red
+                           }
+
 -- Set the colour of a string using POSIX escape codes
-colourise :: Color -> ColorIntensity -> String -> String
-colourise c i str = let sgr = [SetColor Foreground i c]
-                    in setSGRCode sgr ++ str ++ setSGRCode [Reset]
+colourise :: IdrisColour -> String -> String
+colourise (IdrisColour c v u b) str = setSGRCode sgr ++ str ++ setSGRCode [Reset]
+    where sgr = [SetColor Foreground (if v then Vivid else Dull) c] ++
+                (if u then [SetUnderlining SingleUnderline] else []) ++
+                (if b then [SetConsoleIntensity BoldIntensity] else [])
 
 colouriseKwd :: String -> String
-colouriseKwd kwd = setSGRCode [SetUnderlining SingleUnderline
-                              , SetConsoleIntensity BoldIntensity
-                              , SetColor Foreground Vivid Black
-                              ] ++ kwd ++ setSGRCode [Reset]
+colouriseKwd = colourise (keywordColour defaultTheme)
 
 colouriseBound :: String -> String
-colouriseBound = colourise Magenta Vivid
+colouriseBound = colourise (boundVarColour defaultTheme)
 
 colouriseImplicit :: String -> String
-colouriseImplicit str = let sgr = [ SetColor Foreground Vivid Magenta
-                                  , SetUnderlining SingleUnderline
-                                  ]
-                        in setSGRCode sgr ++ str ++ setSGRCode [Reset]
+colouriseImplicit = colourise (implicitColour defaultTheme)
 
 colouriseFun :: String -> String
-colouriseFun = colourise Green Vivid
+colouriseFun = colourise (functionColour defaultTheme)
 
 colouriseType :: String -> String
-colouriseType = colourise Blue Vivid
+colouriseType = colourise (typeColour defaultTheme)
 
 colouriseData :: String -> String
-colouriseData = colourise Red Vivid
+colouriseData = colourise (dataColour defaultTheme)
