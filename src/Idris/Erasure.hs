3c78dcb7c2b030530fbb136da78fc44b838b74e5
Matus Tejiscak <ziman@functor.sk>
2013-12-12 10:42:04 +0000

    Simplify unused arg search.

--- src/Idris/Erasure.hs
+++ src/Idris/Erasure.hs
@@ -16,20 +16,17 @@ findUnusedArgs :: [Name] -> Idris ()
 findUnusedArgs ns = mapM_ traceUnused ns
 
 traceUnused :: Name -> Idris ()
-traceUnused n = do
-    i <- getIState
-    case lookupCtxt n (idris_callgraph i) of
-        [CGInfo args calls scg usedns _] -> do
-            let argpos = zip args [0..]
-            let fargs = concatMap (getFargpos calls) argpos
-            logLvl 3 $ show n ++ " used TRACE: " ++ show fargs
-            recused <- mapM getUsed fargs
-            let fused = nub $ usedns ++ map fst (filter snd recused)
-            logLvl 1 $ show n ++ " used args: " ++ show fused
-            let unusedpos = mapMaybe (getUnused fused) (zip [0..] args)
-            logLvl 1 $ show n ++ " unused args: " ++ show (args, unusedpos)
-            addToCG n (CGInfo args calls scg usedns unusedpos) -- updates
-        _ -> return ()
+traceUnused n = withContext_ idris_callgraph n $
+    \(CGInfo args calls scg usedns _) -> do
+        let argpos = zip args [0..]
+        let fargs = concatMap (getFargpos calls) argpos
+        logLvl 3 $ show n ++ " used TRACE: " ++ show fargs
+        recused <- mapM getUsed fargs
+        let fused = nub $ usedns ++ map fst (filter snd recused)
+        logLvl 1 $ show n ++ " used args: " ++ show fused
+        let unusedpos = mapMaybe (getUnused fused) (zip [0..] args)
+        logLvl 1 $ show n ++ " unused args: " ++ show (args, unusedpos)
+        addToCG n (CGInfo args calls scg usedns unusedpos) -- updates
   where
     getUsed (argn, i, (g, j)) = (,) argn <$> used [(n,i)] g j
     getUnused fused (i,n)
@@ -39,29 +36,29 @@ traceUnused n = do
 used :: [(Name, Int)] -> Name -> Int -> Idris Bool
 used path g j
    | (g, j) `elem` path = return False -- cycle, never used on the way
-   | otherwise
-       = do logLvl 5 $ (show ((g, j) : path))
-            i <- getIState
-            case lookupCtxt g (idris_callgraph i) of
-               [CGInfo args calls scg usedns unused] ->
-                  if (j >= length args)
-                    then -- overapplied, assume used
-                         return True
-                    else do let directuse = args!!j `elem` usedns
-                            let garg = getFargpos calls (args!!j, j)
-                            logLvl 5 $ show (g, j, garg)
-                            recused <- mapM (\ (argn, j, (g', j')) ->
-                                           used ((g,j):path) g' j') garg
-                            -- used on any route from here, or not used recursively
-                            return (directuse || or recused)
-               _ -> return True -- no definition, assume used
+   | otherwise          = withContext idris_callgraph g True $
+        \(CGInfo args calls scg usedns unused) ->
+            if j >= length args
+              then return True -- overapplied, assume used
+              else do
+                logLvl 5 $ (show ((g, j) : path))
+                let directuse = args!!j `elem` usedns
+                let garg = getFargpos calls (args!!j, j)
+                logLvl 5 $ show (g, j, garg)
+                recused <- mapM getUsed garg
+                -- used on any route from here, or not used recursively
+                return (directuse || null recused || or recused)
+  where
+    getUsed (argn, j, (g', j')) = used ((g,j):path) g' j'
 
 getFargpos :: [(Name, [[Name]])] -> (Name, Int) -> [(Name, Int, (Name, Int))]
 getFargpos calls (n, i) = concatMap (getCallArgpos n i) calls
-   where getCallArgpos :: Name -> Int -> (Name, [[Name]]) ->
-                          [(Name, Int, (Name, Int))]
-         getCallArgpos n i (g, args)
-               = let argpos = zip [0..] args in
-                     mapMaybe (\ (j, xs) -> if n `elem` xs then Just (n, i, (g, j))
-                                                           else Nothing) argpos
+  where
+    getCallArgpos :: Name -> Int -> (Name, [[Name]]) -> [(Name, Int, (Name, Int))]
+    getCallArgpos n i (g, args) = mapMaybe (getOne g) (zip [0..] args)
+    
+    getOne :: Name -> (Int, [Name]) -> Maybe (Name, Int, (Name, Int))
+    getOne g (j, xs)
+        | n `elem` xs = Just (n, i, (g, j))
+        | otherwise   = Nothing
 
