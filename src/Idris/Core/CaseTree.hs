6e1cac72fd0660cd26beb6bb1f7ed5a1b65ec9bd
Edwin Brady <ecb10@st-andrews.ac.uk>
2014-01-30 16:31:36 +0000

    (Temporary!) Turn off typecase test
    This needs to be done properly, and currently isn't, which is breaking
    more things than it fixes. Will revisit ASAP...

--- src/Idris/Core/CaseTree.hs
+++ src/Idris/Core/CaseTree.hs
@@ -213,9 +213,12 @@ simpleCase tc cover reflect phase fc argtys cs
                                          ([], numargs, [])
                         t          = CaseDef ns (prune proj (depatt ns' tree)) (fstT st) in
                         if proj then return (stripLambdas t) 
-                                else if checkSameTypes (lstT st) tree
-                                        then return t
-                                        else Error (At fc (Msg "Typecase is not allowed"))
+                                else return t
+-- FIXME: This check is not quite right in some cases, and is breaking
+-- perfectly valid code!
+--                                      if checkSameTypes (lstT st) tree
+--                                         then return t
+--                                         else Error (At fc (Msg "Typecase is not allowed"))
                 Error err -> Error (At fc err)
     where args = map (\i -> sMN i "e") [0..]
           defaultCase True = STerm Erased
