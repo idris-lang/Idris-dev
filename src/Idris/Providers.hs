ef16e5aeadc842a3f04850b96165c6f1aecd6744
David Raymond Christiansen <david@davidchristiansen.dk>
2014-02-03 11:17:15 +0100

    Whitespace cleanup

--- src/Idris/Providers.hs
+++ src/Idris/Providers.hs
@@ -20,13 +20,13 @@ prmod = sNS (sUN "Provide") ["Providers"]
 -- | Handle an error, if the type provider returned an error. Otherwise return the provided term.
 getProvided :: FC -> TT Name -> Idris (TT Name)
 getProvided fc tm | (P _ pioret _, [tp, result]) <- unApply tm
-                  , (P _ nm _, [_, err]) <- unApply result 
+                  , (P _ nm _, [_, err]) <- unApply result
                   , pioret == ioret && nm == ermod
                       = case err of
                           Constant (Str msg) -> ierror . At fc . ProviderError $ msg
                           _ -> ifail "Internal error in type provider, non-normalised error"
                   | (P _ pioret _, [tp, result]) <- unApply tm
-                  , (P _ nm _, [_, res]) <- unApply result 
+                  , (P _ nm _, [_, res]) <- unApply result
                   , pioret == ioret && nm == prmod
                       = return res
                   | otherwise = ifail $ "Internal type provider error: result was not " ++
