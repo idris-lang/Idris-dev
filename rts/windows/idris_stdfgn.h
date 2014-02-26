380a87477bf75fc3bda1dd7fe07765236fdcaf2a
Niklas Larsson <niklas@mm.st>
2014-02-26 20:49:32 +0100

    Split out Windows versions of some rts files
    This allows the introduction of Posix functionality, parallel Windows
    implementations can then be put in the windows/ directory.

---
+++ rts/windows/idris_stdfgn.h
@@ -0,0 +1,24 @@
+#ifndef _IDRISSTDFGN_H
+#define _IDRISSTDFGN_H
+
+// A collection of useful standard functions to be used by the prelude.
+
+void putStr(char* str);
+//char* readStr();
+
+void* fileOpen(char* f, char* mode);
+void fileClose(void* h);
+int fileEOF(void* h);
+int fileError(void* h);
+void fputStr(void*h, char* str);
+
+void* do_popen(const char* cmd, const char* mode);
+int fpoll(void* h);
+
+int idris_eqPtr(void* x, void* y);
+int isNull(void* ptr);
+void* idris_stdin();
+
+char* getEnvPair(int i);
+
+#endif
