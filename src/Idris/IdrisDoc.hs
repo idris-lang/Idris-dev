155e306ef9982cddc56eaf9ae979a90fed022d05
Philip Rasmussen <pjor@itu.dk>
2014-03-25 13:39:42 +0100

    Fixed IdrisDoc errors and added REPL :help descriptions

--- src/Idris/IdrisDoc.hs
+++ src/Idris/IdrisDoc.hs
@@ -1,7 +1,6 @@
-module Idris.IdrisDoc (generateDocs, DocTarget) where
+module Idris.IdrisDoc (generateDocs, DocTarget (..), idrisDocUsage) where
 
 import Idris.AbsSyntax (IState)
-import Idris.Core.TT (Err')
 
 type Failable = Either String
 
@@ -9,13 +8,20 @@ type Failable = Either String
 -- t:   What do generate documentation for
 -- ot:  Where to output documentation
 -- trm: Trace mode?       (will generate documentation for referred modules)
-generateDocs :: IState -> DocTarget -> Filepath -> Bool -> IO (Either String ())
-generateDocs ist t ot trm = error "IdrisDoc has not been implemented yet..."
+generateDocs :: IState -> DocTarget -> FilePath -> Bool -> IO (Either String ())
+generateDocs ist t ot trm = err "IdrisDoc has not been implemented yet..."
 
-error :: String -> IO (Failable ())
-error s = return Left s
+err :: String -> IO (Failable ())
+err s = return $ Left s
 
 -- Target to generate documentation for
 -- Namespace ns: Generate documentation for the namespace identified by 'ns'
 -- Dir d:        Generate documentation for all namespaces in the directory 'd'
 data DocTarget = Namespace String | Dir FilePath
+
+idrisDocUsage = "Usage:   :mkdoc [notrace] [ns | dir] [TARGET]\n\n" ++
+                "notrace: Do not generate IdrisDoc for addtionally referred namespaces.\n" ++
+                "ns:      TARGET is a loaded namespace. TARGET required.\n" ++
+                "dir:     TARGET is a directory to search recursively for .idr files. TARGET required.\n" ++
+                "TARGET:  What to generate IdrisDoc for. By default interpreted as a namespace.\n" ++
+                "<none>:  Same as ':mkdoc dir .'"
