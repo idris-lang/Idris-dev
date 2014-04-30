4168f0579a3a709ccea3189893c2cd2535ff68b2
Paul Koerbitz <paul.koerbitz@theppro.com>
2014-05-01 01:06:51 +0200

    Remove deprecatd classInfo method from REPL.hs

--- src/Idris/REPL.hs
+++ src/Idris/REPL.hs
@@ -1096,31 +1096,6 @@ process h fn (MakeDoc s) =
                         Left  err -> iPrintError err
 
 
-classInfo :: ClassInfo -> Idris (Doc OutputAnnotation)
-classInfo ci = do ist <- getIState
-                  ctxt <- getContext
-                  return $
-                    text "Parameters:" <+>
-                    hsep (punctuate comma (map (prettyName False params') params)) <>
-                    line <> line <>
-                    text "Methods:" <> line <>
-                    indent 2 (vsep (map (dumpMethod ist) (class_methods ci))) <> line <> line <>
-                    text "Default superclass instances:" <>
-                    line <>
-                    indent 2 (vsep (map (dumpDefaultInstance ist) (class_default_superclasses ci))) <>
-                    line <>
-                    text "Instances:" <> line <>
-                    indent 2 (vsep (map (dumpInstance ist ctxt) (class_instances ci)))
-  where dumpMethod :: IState -> (Name, (FnOpts, PTerm)) -> Doc OutputAnnotation
-        dumpMethod ist (n, (_, t)) = prettyName False [] n <+> colon <+> pp ist t
-        dumpDefaultInstance :: IState -> PDecl -> Doc OutputAnnotation
-        dumpDefaultInstance ist (PInstance _ _ _ _ _ t _ _) = pp ist t
-        dumpInstance :: IState -> Context -> Name -> Doc OutputAnnotation
-        dumpInstance ist ctxt n = pp ist $ delabTy ist n
-        params = class_params ci
-        params' = zip params (repeat False)
-        pp ist = pprintPTerm (opt_showimp (idris_options ist)) params' []
-
 showTotal :: Totality -> IState -> String
 showTotal t@(Partial (Other ns)) i
    = show t ++ "\n\t" ++ showSep "\n\t" (map (showTotalN i) ns)
