dae7d7d2243fa69563b27619ee59f6890d8187c1
Edwin Brady <ecb10@st-andrews.ac.uk>
2014-05-10 15:17:50 +0100

    Move interactive commands to their own module

--- src/Idris/REPL.hs
+++ src/Idris/REPL.hs
@@ -30,6 +30,7 @@ import Idris.Inliner
 import Idris.CaseSplit
 import Idris.DeepSeq
 import Idris.Output
+import Idris.Interactive
 import Idris.WhoCalls
 
 import Paths_idris
@@ -698,186 +699,18 @@ process h fn (DebugInfo n)
         when (not (null cg')) $ do iputStrLn "Call graph:\n"
                                    iputStrLn (show cg')
 process h fn (Search t) = iPrintError "Not implemented"
--- FIXME: There is far too much repetition in the cases below!
-process h fn (CaseSplitAt updatefile l n)
-   = do src <- runIO $ readFile fn
-        res <- splitOnLine l n fn
-        iLOG (showSep "\n" (map show res))
-        let (before, (ap : later)) = splitAt (l-1) (lines src)
-        res' <- replaceSplits ap res
-        let new = concat res'
-        if updatefile
-          then do let fb = fn ++ "~" -- make a backup!
-                  runIO $ writeFile fb (unlines before ++ new ++ unlines later)
-                  runIO $ copyFile fb fn
-          else -- do ihputStrLn h (show res)
-            ihPrintResult h new
+process h fn (CaseSplitAt updatefile l n) 
+    = caseSplitAt h fn updatefile l n
 process h fn (AddClauseFrom updatefile l n)
-   = do src <- runIO $ readFile fn
-        let (before, tyline : later) = splitAt (l-1) (lines src)
-        let indent = getIndent 0 (show n) tyline
-        cl <- getClause l n fn
-        -- add clause before first blank line in 'later'
-        let (nonblank, rest) = span (not . all isSpace) (tyline:later)
-        if updatefile
-          then do let fb = fn ++ "~"
-                  runIO $ writeFile fb (unlines (before ++ nonblank) ++
-                                        replicate indent ' ' ++
-                                        cl ++ "\n" ++
-                                        unlines rest)
-                  runIO $ copyFile fb fn
-          else ihPrintResult h cl
-    where
-       getIndent i n [] = 0
-       getIndent i n xs | take (length n) xs == n = i
-       getIndent i n (x : xs) = getIndent (i + 1) n xs
+    = addClauseFrom h fn updatefile l n
 process h fn (AddProofClauseFrom updatefile l n)
-   = do src <- runIO $ readFile fn
-        let (before, tyline : later) = splitAt (l-1) (lines src)
-        let indent = getIndent 0 (show n) tyline
-        cl <- getProofClause l n fn
-        -- add clause before first blank line in 'later'
-        let (nonblank, rest) = span (not . all isSpace) (tyline:later)
-        if updatefile
-          then do let fb = fn ++ "~"
-                  runIO $ writeFile fb (unlines (before ++ nonblank) ++
-                                        replicate indent ' ' ++
-                                        cl ++ "\n" ++
-                                        unlines rest)
-                  runIO $ copyFile fb fn
-          else ihPrintResult h cl
-    where
-       getIndent i n [] = 0
-       getIndent i n xs | take (length n) xs == n = i
-       getIndent i n (x : xs) = getIndent (i + 1) n xs
+    = addProofClauseFrom h fn updatefile l n
 process h fn (AddMissing updatefile l n)
-   = do src <- runIO $ readFile fn
-        let (before, tyline : later) = splitAt (l-1) (lines src)
-        let indent = getIndent 0 (show n) tyline
-        i <- getIState
-        cl <- getInternalApp fn l
-        let n' = getAppName cl
-
-        extras <- case lookupCtxt n' (idris_patdefs i) of
-                       [] -> return ""
-                       [(_, tms)] -> do tms' <- nameMissing tms
-                                        showNew (show n ++ "_rhs") 1 indent tms'
-        let (nonblank, rest) = span (not . all isSpace) (tyline:later)
-        if updatefile
-          then do let fb = fn ++ "~"
-                  runIO $ writeFile fb (unlines (before ++ nonblank)
-                                        ++ extras ++ unlines rest)
-                  runIO $ copyFile fb fn
-          else ihPrintResult h extras
-    where showPat = show . stripNS
-          stripNS tm = mapPT dens tm where
-              dens (PRef fc n) = PRef fc (nsroot n)
-              dens t = t
-
-          nsroot (NS n _) = nsroot n
-          nsroot (SN (WhereN _ _ n)) = nsroot n
-          nsroot n = n
-
-          getAppName (PApp _ r _) = getAppName r
-          getAppName (PRef _ r) = r
-          getAppName _ = n
-
-          showNew nm i ind (tm : tms)
-                        = do (nm', i') <- getUniq nm i
-                             rest <- showNew nm i' ind tms
-                             return (replicate ind ' ' ++
-                                     showPat tm ++ " = ?" ++ nm' ++
-                                     "\n" ++ rest)
-          showNew nm i _ [] = return ""
-
-          getIndent i n [] = 0
-          getIndent i n xs | take (length n) xs == n = i
-          getIndent i n (x : xs) = getIndent (i + 1) n xs
-
+    = addMissing h fn updatefile l n
 process h fn (MakeWith updatefile l n)
-   = do src <- runIO $ readFile fn
-        let (before, tyline : later) = splitAt (l-1) (lines src)
-        let ind = getIndent tyline
-        let with = mkWith tyline n
-        -- add clause before first blank line in 'later',
-        -- or (TODO) before first line with same indentation as tyline
-        let (nonblank, rest) = span (\x -> not (all isSpace x) &&
-                                           not (ind == getIndent x)) later
-        if updatefile then
-           do let fb = fn ++ "~"
-              runIO $ writeFile fb (unlines (before ++ nonblank)
-                                        ++ with ++ "\n" ++
-                                    unlines rest)
-              runIO $ copyFile fb fn
-           else ihPrintResult h with
-  where getIndent s = length (takeWhile isSpace s)
-
+    = makeWith h fn updatefile l n
 process h fn (DoProofSearch updatefile rec l n hints)
-    = do src <- runIO $ readFile fn
-         let (before, tyline : later) = splitAt (l-1) (lines src)
-         ctxt <- getContext
-         mn <- case lookupNames n ctxt of
-                    [x] -> return x
-                    [] -> return n
-                    ns -> ierror (CantResolveAlts (map show ns))
-         i <- getIState
-         let (top, envlen, _) = case lookup mn (idris_metavars i) of
-                                  Just (t, e, False) -> (t, e, False)
-                                  _ -> (Nothing, 0, True)
-         let fc = fileFC fn
-         let body t = PProof [Try (TSeq Intros (ProofSearch rec t hints))
-                                  (ProofSearch rec t hints)]
-         let def = PClause fc mn (PRef fc mn) [] (body top) []
-         newmv <- idrisCatch
-             (do elabDecl' EAll toplevel (PClauses fc [] mn [def])
-                 (tm, ty) <- elabVal toplevel False (PRef fc mn)
-                 ctxt <- getContext
-                 i <- getIState
-                 return . flip displayS "" . renderPretty 1.0 80 $
-                   pprintPTerm defaultPPOption [] [] (idris_infixes i)
-                     (stripNS
-                        (dropCtxt envlen
-                           (delab i (specialise ctxt [] [(mn, 1)] tm)))))
-             (\e -> return ("?" ++ show n))
-         if updatefile then
-            do let fb = fn ++ "~"
-               runIO $ writeFile fb (unlines before ++
-                                     updateMeta False tyline (show n) newmv ++ "\n"
-                                       ++ unlines later)
-               runIO $ copyFile fb fn
-            else ihPrintResult h newmv
-    where dropCtxt 0 sc = sc
-          dropCtxt i (PPi _ _ _ sc) = dropCtxt (i - 1) sc
-          dropCtxt i (PLet _ _ _ sc) = dropCtxt (i - 1) sc
-          dropCtxt i (PLam _ _ sc) = dropCtxt (i - 1) sc
-          dropCtxt _ t = t
-
-          stripNS tm = mapPT dens tm where
-              dens (PRef fc n) = PRef fc (nsroot n)
-              dens t = t
-
-          nsroot (NS n _) = nsroot n
-          nsroot (SN (WhereN _ _ n)) = nsroot n
-          nsroot n = n
-
-          updateMeta brack ('?':cs) n new
-            | length cs >= length n
-              = case splitAt (length n) cs of
-                     (mv, c:cs) ->
-                          if ((isSpace c || c == ')' || c == '}') && mv == n)
-                             then addBracket brack new ++ (c : cs)
-                             else '?' : mv ++ c : updateMeta True cs n new
-                     (mv, []) -> if (mv == n) then addBracket brack new else '?' : mv
-          updateMeta brack ('=':cs) n new = '=':updateMeta False cs n new
-          updateMeta brack (c:cs) n new 
-              = c : updateMeta (brack || not (isSpace c)) cs n new
-          updateMeta brack [] n new = ""
-
-          addBracket False new = new
-          addBracket True new@('(':xs) | last xs == ')' = new
-          addBracket True new | any isSpace new = '(' : new ++ ")"
-                              | otherwise = new
-
+    = doProofSearch h fn updatefile rec l n hints
 process h fn (Spec t)
                     = do (tm, ty) <- elabVal toplevel False t
                          ctxt <- getContext
