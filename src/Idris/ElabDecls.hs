f02f3c02cbc1b88e0ab2dfdce5de33ae28fad715
Edwin Brady <ecb10@st-andrews.ac.uk>
2014-05-12 02:00:24 +0100

    Check if class is meant to be empty...
    ...before treating an empty class definition as a declaration rather
    than a definition.

--- src/Idris/ElabDecls.hs
+++ src/Idris/ElabDecls.hs
@@ -2041,7 +2041,8 @@ elabInstance info syn what fc cs n ps t expn ds = do
                            (CantResolveAlts (map (show . fst) cs))
     let constraint = PApp fc (PRef fc n) (map pexp ps)
     let iname = mkiname n ps expn
-    when (what /= EDefns || null ds) $ do
+    let emptyclass = null (class_methods ci)
+    when (what /= EDefns || (null ds && not emptyclass)) $ do
          nty <- elabType' True info syn emptyDocstring [] fc [] iname t
          -- if the instance type matches any of the instances we have already,
          -- and it's not a named instance, then it's overlapping, so report an error
@@ -2050,7 +2051,7 @@ elabInstance info syn what fc cs n ps t expn ds = do
                                 (class_instances ci)
                           addInstance intInst n iname
             Just _ -> addInstance intInst n iname
-    when (what /= ETypes && (not (null ds))) $ do 
+    when (what /= ETypes && (not (null ds && not emptyclass))) $ do 
          let ips = zip (class_params ci) ps
          let ns = case n of
                     NS n ns' -> ns'
