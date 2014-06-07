71fae2801f00bef26b901567d9d055b623b9869c
Jan Bessai <Jan.Bessai@tu-dortmund.de>
2014-06-07 22:01:16 +0200

    fix missing tests

--- test/reg029/Reg029Wrapper.java
+++ test/reg029/Reg029Wrapper.java
@@ -7,7 +7,7 @@ import java.util.Map;
  * into the standard set of properties.
  */
 public class Reg029Wrapper {
-  public static void main(String [] args) {
+  public static void main(String args[]) {
     Map<String, String> env = System.getenv();
     for (String key : env.keySet()) {
       System.setProperty(key, env.get(key));
