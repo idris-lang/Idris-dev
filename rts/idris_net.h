b92213c36d35caff1b32a43625de8eda48733acb
Edwin Brady <ecb10@st-andrews.ac.uk>
2014-06-14 14:19:42 +0100

    Update Network library
    (Taken from @SimonJF's IdrisNet2 library at
    https://github.com/SimonJF/IdrisNet2)

--- rts/idris_net.h
+++ rts/idris_net.h
@@ -1,10 +1,29 @@
 #ifndef IDRISNET_H
 #define IDRISNET_H
 
+#include <errno.h>
+#include <netdb.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 typedef struct idrnet_recv_result {
     int result;
     void* payload;
 } idrnet_recv_result;
+
+// Same type of thing as idrnet_recv_result, but for UDP, so stores some 
+// address information
+typedef struct idrnet_recvfrom_result {
+    int result;
+    void* payload;
+    struct sockaddr_storage* remote_addr;
+} idrnet_recvfrom_result;
 
 // Memory management functions
 void* idrnet_malloc(int size);
@@ -22,6 +41,7 @@ int idrnet_connect(int sockfd, int family, int socket_type, char* host, int port
 // Accessor functions for struct_sockaddr
 int idrnet_sockaddr_family(void* sockaddr);
 char* idrnet_sockaddr_ipv4(void* sockaddr);
+int idrnet_sockaddr_ipv4_port(void* sockaddr);
 void* idrnet_create_sockaddr();
 
 // Accept
@@ -37,8 +57,29 @@ void* idrnet_recv(int sockfd, int len);
 // Receives directly into a buffer
 int idrnet_recv_buf(int sockfd, void* buf, int len);
 
+// UDP Send
+int idrnet_sendto(int sockfd, char* data, char* host, int port, int family);
+int idrnet_sendto_buf(int sockfd, void* buf, int buf_len, char* host, int port, int family);
+
+
+// UDP Receive
+void* idrnet_recvfrom(int sockfd, int len);
+void* idrnet_recvfrom_buf(int sockfd, void* buf, int len);
+
 // Receive structure accessors
 int idrnet_get_recv_res(void* res_struct);
-void* idrnet_get_recv_payload(void* res_struct);
+char* idrnet_get_recv_payload(void* res_struct);
 void idrnet_free_recv_struct(void* res_struct);
+
+// Recvfrom structure accessors
+int idrnet_get_recvfrom_res(void* res_struct);
+char* idrnet_get_recvfrom_payload(void* res_struct);
+void* idrnet_get_recvfrom_sockaddr(void* res_struct);
+void idrnet_free_recvfrom_struct(void* res_struct);
+
+
+int idrnet_getaddrinfo(struct addrinfo** address_res, char* host, 
+    int port, int family, int socket_type);
+
+
 #endif
