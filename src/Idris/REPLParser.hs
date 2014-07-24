fb2d914e4c5b867699a527cd14ffedf5b8296b66
Ahmad Salim Al-Sibahi <asal@itu.dk>
2014-07-24 12:23:30 +0200

    Removed unnecessary backtracking in REPL command parsing making errors harder to understand

--- src/Idris/REPLParser.hs
+++ src/Idris/REPLParser.hs
@@ -27,104 +27,104 @@ parseCmd :: IState -> String -> String -> Result Command
 parseCmd i inputname = P.runparser pCmd i inputname
 
 cmd :: [String] -> P.IdrisParser ()
-cmd xs = do P.lchar ':'; docmd (sortBy (\x y -> compare (length y) (length x)) xs)
+cmd xs = try (do P.lchar ':'; docmd (sortBy (\x y -> compare (length y) (length x)) xs))
     where docmd [] = fail "No such command"
           docmd (x:xs) = try (discard (P.symbol x)) <|> docmd xs
 
 pCmd :: P.IdrisParser Command
-pCmd = do P.whiteSpace; try (do cmd ["q", "quit"]; eof; return Quit)
-              <|> try (do cmd ["h", "?", "help"]; eof; return Help)
-              <|> try (do cmd ["w", "warranty"]; eof; return Warranty)
-              <|> try (do cmd ["r", "reload"]; eof; return Reload)
-              <|> try (do cmd ["module"]; f <- P.identifier; eof;
-                          return (ModImport (toPath f)))
-              <|> try (do cmd ["e", "edit"]; eof; return Edit)
-              <|> try (do cmd ["exec", "execute"]; eof; return Execute)
-              <|> try (do cmd ["c", "compile"]
-                          i <- get
-                          c <- option (opt_codegen $ idris_options i) codegenOption
-                          f <- P.identifier
-                          eof
-                          return (Compile c f))
-              <|> try (do cmd ["proofs"]; eof; return Proofs)
-              <|> try (do cmd ["rmproof"]; n <- P.name; eof; return (RmProof n))
-              <|> try (do cmd ["showproof"]; n <- P.name; eof; return (ShowProof n))
-              <|> try (do cmd ["log"]; i <- P.natural; eof; return (LogLvl (fromIntegral i)))
-              <|> try (do cmd ["let"]
-                          defn <- concat <$> many (P.decl defaultSyntax)
-                          return (NewDefn defn))
-              <|> try (do cmd ["lto", "loadto"];
-                          toline <- P.natural
-                          f <- many anyChar;
-                          return (Load f (Just (fromInteger toline))))
-              <|> try (do cmd ["l", "load"]; f <- many anyChar;
-                          return (Load f Nothing))
-              <|> try (do cmd ["cd"]; f <- many anyChar; return (ChangeDirectory f))
-              <|> try (do cmd ["spec"]; P.whiteSpace; t <- P.fullExpr defaultSyntax; return (Spec t))
-              <|> try (do cmd ["hnf"]; P.whiteSpace; t <- P.fullExpr defaultSyntax; return (HNF t))
-              <|> try (do cmd ["inline"]; P.whiteSpace; t <- P.fullExpr defaultSyntax; return (TestInline t))
-              <|> try (do cmd ["doc"]; c <- P.constant; eof; return (DocStr (Right c)))
-              <|> try (do cmd ["doc"]; n <- (P.fnName <|> (P.string "_|_" >> return falseTy)); eof; return (DocStr (Left n)))
-              <|> try (do cmd ["d", "def"]; P.whiteSpace; n <- P.fnName; eof; return (Defn n))
-              <|> try (do cmd ["total"]; do n <- P.fnName; eof; return (TotCheck n))
-              <|> try (do cmd ["t", "type"]; do P.whiteSpace; t <- P.fullExpr defaultSyntax; return (Check t))
-              <|> try (do cmd ["u", "universes"]; eof; return Universes)
-              <|> try (do cmd ["di", "dbginfo"]; n <- P.fnName; eof; return (DebugInfo n))
-              <|> try (do cmd ["miss", "missing"]; n <- P.fnName; eof; return (Missing n))
+pCmd = do P.whiteSpace; do cmd ["q", "quit"]; eof; return Quit
+              <|> do cmd ["h", "?", "help"]; eof; return Help
+              <|> do cmd ["w", "warranty"]; eof; return Warranty
+              <|> do cmd ["r", "reload"]; eof; return Reload
+              <|> do cmd ["module"]; f <- P.identifier; eof;
+                          return (ModImport (toPath f))
+              <|> do cmd ["e", "edit"]; eof; return Edit
+              <|> do cmd ["exec", "execute"]; eof; return Execute
+              <|> do cmd ["c", "compile"]
+                     i <- get
+                     c <- option (opt_codegen $ idris_options i) codegenOption
+                     f <- P.identifier
+                     eof
+                     return (Compile c f)
+              <|> do cmd ["proofs"]; eof; return Proofs
+              <|> do cmd ["rmproof"]; n <- P.name; eof; return (RmProof n)
+              <|> do cmd ["showproof"]; n <- P.name; eof; return (ShowProof n)
+              <|> do cmd ["log"]; i <- P.natural; eof; return (LogLvl (fromIntegral i))
+              <|> do cmd ["let"]
+                     defn <- concat <$> many (P.decl defaultSyntax)
+                     return (NewDefn defn)
+              <|> do cmd ["lto", "loadto"];
+                     toline <- P.natural
+                     f <- many anyChar;
+                     return (Load f (Just (fromInteger toline)))
+              <|> do cmd ["l", "load"]; f <- many anyChar;
+                     return (Load f Nothing)
+              <|> do cmd ["cd"]; f <- many anyChar; return (ChangeDirectory f)
+              <|> do cmd ["spec"]; P.whiteSpace; t <- P.fullExpr defaultSyntax; return (Spec t)
+              <|> do cmd ["hnf"]; P.whiteSpace; t <- P.fullExpr defaultSyntax; return (HNF t)
+              <|> do cmd ["inline"]; P.whiteSpace; t <- P.fullExpr defaultSyntax; return (TestInline t)
+              <|> do c <- try (cmd ["doc"] *> P.constant); eof; return (DocStr (Right c))
+              <|> do cmd ["doc"]; n <- (P.fnName <|> (P.string "_|_" >> return falseTy)); eof; return (DocStr (Left n))
+              <|> do cmd ["d", "def"]; P.whiteSpace; n <- P.fnName; eof; return (Defn n)
+              <|> do cmd ["total"]; do n <- P.fnName; eof; return (TotCheck n)
+              <|> do cmd ["t", "type"]; do P.whiteSpace; t <- P.fullExpr defaultSyntax; return (Check t)
+              <|> do cmd ["u", "universes"]; eof; return Universes
+              <|> do cmd ["di", "dbginfo"]; n <- P.fnName; eof; return (DebugInfo n)
+              <|> do cmd ["miss", "missing"]; n <- P.fnName; eof; return (Missing n)
               <|> try (do cmd ["dynamic"]; eof; return ListDynamic)
-              <|> try (do cmd ["dynamic"]; l <- many anyChar; return (DynamicLink l))
-              <|> try (do cmd ["color", "colour"]; pSetColourCmd)
-              <|> try (do cmd ["set"]; o <- pOption; return (SetOpt o))
-              <|> try (do cmd ["unset"]; o <- pOption; return (UnsetOpt o))
-              <|> try (do cmd ["s", "search"]; P.whiteSpace;
-                          t <- P.typeExpr (defaultSyntax { implicitAllowed = True }); return (Search t))
-              <|> try (do cmd ["cs", "casesplit"]; P.whiteSpace;
-                          upd <- option False (do P.lchar '!'; return True)
-                          l <- P.natural; n <- P.name;
-                          return (CaseSplitAt upd (fromInteger l) n))
-              <|> try (do cmd ["apc", "addproofclause"]; P.whiteSpace;
-                          upd <- option False (do P.lchar '!'; return True)
-                          l <- P.natural; n <- P.name;
-                          return (AddProofClauseFrom upd (fromInteger l) n))
-              <|> try (do cmd ["ac", "addclause"]; P.whiteSpace;
-                          upd <- option False (do P.lchar '!'; return True)
-                          l <- P.natural; n <- P.name;
-                          return (AddClauseFrom upd (fromInteger l) n))
-              <|> try (do cmd ["am", "addmissing"]; P.whiteSpace;
-                          upd <- option False (do P.lchar '!'; return True)
-                          l <- P.natural; n <- P.name;
-                          return (AddMissing upd (fromInteger l) n))
-              <|> try (do cmd ["mw", "makewith"]; P.whiteSpace;
-                          upd <- option False (do P.lchar '!'; return True)
-                          l <- P.natural; n <- P.name;
-                          return (MakeWith upd (fromInteger l) n))
-              <|> try (do cmd ["ml", "makelemma"]; P.whiteSpace;
-                          upd <- option False (do P.lchar '!'; return True)
-                          l <- P.natural; n <- P.name;
-                          return (MakeLemma upd (fromInteger l) n))
-              <|> try (do cmd ["ps", "proofsearch"]; P.whiteSpace;
-                          upd <- option False (do P.lchar '!'; return True)
-                          l <- P.natural; n <- P.name;
-                          hints <- many P.fnName
-                          return (DoProofSearch upd True (fromInteger l) n hints))
-              <|> try (do cmd ["ref", "refine"]; P.whiteSpace;
-                          upd <- option False (do P.lchar '!'; return True)
-                          l <- P.natural; n <- P.name;
-                          hint <- P.fnName
-                          return (DoProofSearch upd False (fromInteger l) n [hint]))
-              <|> try (do cmd ["p", "prove"]; n <- P.name; eof; return (Prove n))
-              <|> try (do cmd ["m", "metavars"]; eof; return Metavars)
-              <|> try (do cmd ["a", "addproof"]; do n <- option Nothing (do x <- P.name;
-                                                                            return (Just x))
-                                                    eof; return (AddProof n))
-              <|> try (do cmd ["x"]; P.whiteSpace; t <- P.fullExpr defaultSyntax; return (ExecVal t))
-              <|> try (do cmd ["patt"]; P.whiteSpace; t <- P.fullExpr defaultSyntax; return (Pattelab t))
-              <|> try (do cmd ["errorhandlers"]; eof ; return ListErrorHandlers)
-              <|> try (do cmd ["consolewidth"]; w <- pConsoleWidth ; return (SetConsoleWidth w))
-              <|> try (do cmd ["apropos"]; str <- many anyChar ; return (Apropos str))
-              <|> try (do cmd ["wc", "whocalls"]; P.whiteSpace; n <- P.fnName ; return (WhoCalls n))
-              <|> try (do cmd ["cw", "callswho"]; P.whiteSpace; n <- P.fnName ; return (CallsWho n))
-              <|> try (do cmd ["mkdoc"]; str <- many anyChar; return (MakeDoc str))
+              <|> do cmd ["dynamic"]; l <- many anyChar; return (DynamicLink l)
+              <|> do cmd ["color", "colour"]; pSetColourCmd
+              <|> do cmd ["set"]; o <- pOption; return (SetOpt o)
+              <|> do cmd ["unset"]; o <- pOption; return (UnsetOpt o)
+              <|> do cmd ["s", "search"]; P.whiteSpace;
+                          t <- P.typeExpr (defaultSyntax { implicitAllowed = True }); return (Search t)
+              <|> do cmd ["cs", "casesplit"]; P.whiteSpace;
+                     upd <- option False (do P.lchar '!'; return True)
+                     l <- P.natural; n <- P.name;
+                     return (CaseSplitAt upd (fromInteger l) n)
+              <|> do cmd ["apc", "addproofclause"]; P.whiteSpace;
+                     upd <- option False (do P.lchar '!'; return True)
+                     l <- P.natural; n <- P.name;
+                     return (AddProofClauseFrom upd (fromInteger l) n)
+              <|> do cmd ["ac", "addclause"]; P.whiteSpace;
+                     upd <- option False (do P.lchar '!'; return True)
+                     l <- P.natural; n <- P.name;
+                     return (AddClauseFrom upd (fromInteger l) n)
+              <|> do cmd ["am", "addmissing"]; P.whiteSpace;
+                     upd <- option False (do P.lchar '!'; return True)
+                     l <- P.natural; n <- P.name;
+                     return (AddMissing upd (fromInteger l) n)
+              <|> do cmd ["mw", "makewith"]; P.whiteSpace;
+                     upd <- option False (do P.lchar '!'; return True)
+                     l <- P.natural; n <- P.name;
+                     return (MakeWith upd (fromInteger l) n)
+              <|> do cmd ["ml", "makelemma"]; P.whiteSpace;
+                     upd <- option False (do P.lchar '!'; return True)
+                     l <- P.natural; n <- P.name;
+                     return (MakeLemma upd (fromInteger l) n)
+              <|> do cmd ["ps", "proofsearch"]; P.whiteSpace;
+                     upd <- option False (do P.lchar '!'; return True)
+                     l <- P.natural; n <- P.name;
+                     hints <- many P.fnName
+                     return (DoProofSearch upd True (fromInteger l) n hints)
+              <|> do cmd ["ref", "refine"]; P.whiteSpace;
+                     upd <- option False (do P.lchar '!'; return True)
+                     l <- P.natural; n <- P.name;
+                     hint <- P.fnName
+                     return (DoProofSearch upd False (fromInteger l) n [hint])
+              <|> do cmd ["p", "prove"]; n <- P.name; eof; return (Prove n)
+              <|> do cmd ["m", "metavars"]; eof; return Metavars
+              <|> do cmd ["a", "addproof"]; do n <- option Nothing (do x <- P.name;
+                                                                       return (Just x))
+                                               eof; return (AddProof n)
+              <|> do cmd ["x"]; P.whiteSpace; t <- P.fullExpr defaultSyntax; return (ExecVal t)
+              <|> do cmd ["patt"]; P.whiteSpace; t <- P.fullExpr defaultSyntax; return (Pattelab t)
+              <|> do cmd ["errorhandlers"]; eof ; return ListErrorHandlers
+              <|> do cmd ["consolewidth"]; w <- pConsoleWidth ; return (SetConsoleWidth w)
+              <|> do cmd ["apropos"]; str <- many anyChar ; return (Apropos str)
+              <|> do cmd ["wc", "whocalls"]; P.whiteSpace; n <- P.fnName ; return (WhoCalls n)
+              <|> do cmd ["cw", "callswho"]; P.whiteSpace; n <- P.fnName ; return (CallsWho n)
+              <|> do cmd ["mkdoc"]; str <- many anyChar; return (MakeDoc str)
               <|> do P.whiteSpace; do eof; return NOP
                              <|> do t <- P.fullExpr defaultSyntax; return (Eval t)
 
