15101555d36e356510362a80c68b2f6ccdf6bb0e
Heather <heather@live.ru>
2015-02-12 10:43:26 +0400

    clean includes from c file

--- rts/idris_rts.c
+++ rts/idris_rts.c
@@ -1,11 +1,4 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdarg.h>
 #include <assert.h>
-#ifdef HAS_PTHREAD
-#include <pthread.h>
-#endif
 
 #include "idris_rts.h"
 #include "idris_gc.h"
