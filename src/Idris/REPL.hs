8688f43b3e91be060b94026ee7cba5337a7324ea
David Raymond Christiansen <david@davidchristiansen.dk>
2015-02-27 13:17:32 +0100

    Fix :unlet and the IDE protocol
    Previously, :unlet was stating that it had more output when it was done,
    which could lead IDE clients to hang while waiting for it.
    
    Fixes https://github.com/idris-hackers/idris-mode/issues/299

--- src/Idris/REPL.hs
+++ src/Idris/REPL.hs
@@ -821,7 +821,7 @@ process fn (Undefine names) = undefine names
     -- Keep track of which names you've removed so you can
     -- print them out to the user afterward
     undefine names = undefine' names []
-    undefine' [] list = do iRenderOutput $ printUndefinedNames list
+    undefine' [] list = do iRenderResult $ printUndefinedNames list
                            return ()
     undefine' (n:names) already = do
       allDefined <- idris_repl_defs `fmap` get
