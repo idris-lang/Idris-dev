742cf9c8fbd2672af2bab3633ba3ba180c45c272
David Raymond Christiansen <david@davidchristiansen.dk>
2015-03-19 18:58:20 +0100

    Add script to put library docs in a tarball.
    Fixes #1987, because now Edwin will upload them.

--- mkpkg.sh
+++ mkpkg.sh
@@ -10,6 +10,15 @@ git tag v$VERSION -a
 
 cabal sdist
 
+# Generate Idris library docs and put them in lib_docs.tar.gz in the root
+make lib_doc
+DOCDIR=`mktemp -d /tmp/docsXXXXX`
+cp -r libs/base/base_doc "$DOCDIR"
+cp -r libs/prelude/prelude_doc "$DOCDIR"
+cp -r libs/effects/effects_doc "$DOCDIR"
+cp -r libs/contrib/contrib_doc "$DOCDIR"
+tar -czvf lib_docs.tar.gz -C "$DOCDIR" prelude_doc base_doc effects_doc contrib_doc
+
 cabal configure --prefix=/usr/local
 cabal build
 cabal copy --destdir=/tmp/idris-pkg/
