572d70f17cbc8fc2577258fe13276342a94eb48b
Jan de Muijnck-Hughes <jfdm@st-andrews.ac.uk>
2015-03-31 10:57:16 +0100

    Modified conf.py to work locally if rtd-theme is not installed.

--- docs/conf.py
+++ docs/conf.py
@@ -115,11 +115,12 @@ todo_include_todos = True
 html_theme = "default"
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-
+    try:
+        import sphinx_rtd_theme
+        html_theme = 'sphinx_rtd_theme'
+        html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    except ImportError:
+        html_theme = "default"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
