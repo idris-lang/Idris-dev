2c91acc319bac05467a63030dd4729f55b872821
Zack Grannan <zgrannan@gmail.com>
2015-10-05 21:40:22 -0700

    Changed to not affect normal expression parsing

--- src/Idris/REPLParser.hs
+++ src/Idris/REPLParser.hs
@@ -163,10 +163,9 @@ pCmd :: P.IdrisParser (Either String Command)
 pCmd = choice [ do c <- cmd names; parser c 
               | (names, _, _, parser) <- parserCommandsForHelp ++ parserCommands ]
      <|> unrecognized 
-     <|> nop 
+     <|> nop
      <|> eval
     where nop = do eof; return (Right NOP)
-          eval = exprArg Eval ""
           unrecognized = do
               P.lchar ':'
               cmd <- many anyChar
@@ -194,6 +193,11 @@ noArgs cmd name = do
 
     emptyArgs <|> failure
 
+eval :: P.IdrisParser (Either String Command)
+eval = do
+  t <- P.fullExpr defaultSyntax
+  return $ Right (Eval t)
+
 exprArg :: (PTerm -> Command) -> String -> P.IdrisParser (Either String Command)
 exprArg cmd name = do
     let noArg = do
@@ -201,9 +205,9 @@ exprArg cmd name = do
         return $ Left ("Usage is :" ++ name ++ " <expression>")
 
     let justOperator = do
-        (op, fc) <- P.maybeWithNS P.operatorFC False []
+        (op, fc) <- P.operatorFC
         eof
-        return $ Right $ cmd (PRef fc [] op)
+        return $ Right $ cmd (PRef fc [] (sUN op))
 
     let properArg = do
         t <- P.fullExpr defaultSyntax
