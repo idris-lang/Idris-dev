0917cf8864ea4a41ab9d94f9c3a9e9d2a72159c3
Eduard Nicodei <eddnicodei@gmail.com>
2016-05-15 21:11:11 +0200

    #3170: Use gmake if compiling on OpenBSD.

--- Setup.hs
+++ Setup.hs
@@ -38,7 +38,7 @@ idrisCmd local = ".." </> ".." </>  buildDir local </>  "idris" </>  "idris"
 -- Make Commands
 
 -- use GNU make on FreeBSD
-#if defined(freebsd_HOST_OS) || defined(dragonfly_HOST_OS)
+#if defined(freebsd_HOST_OS) || defined(dragonfly_HOST_OS) || defined(openbsd_HOST_OS)
 mymake = "gmake"
 #else
 mymake = "make"
