4abfa34db03189c847084cd2517375aa43e9e069
Timo Petteri Sinnemäki <timo.sinnemaki@helsinki.fi>
2016-06-21 00:24:13 +0300

    Added native libs to LD_LIBRARY_PATH due to ghci issue.

--- stack-shell.nix
+++ stack-shell.nix
@@ -25,4 +25,7 @@ in stdenv.mkDerivation {
     (acc: lib:
       " --extra-lib-dirs=${lib}/lib --extra-include-dirs=${lib}/include" + acc)
     "" native_libs;
+
+  # Needed if one wants to use ghci, due to https://ghc.haskell.org/trac/ghc/ticket/11042
+  LD_LIBRARY_PATH = builtins.concatStringsSep ":" (map (lib: lib.out + "/lib") native_libs);
 }
