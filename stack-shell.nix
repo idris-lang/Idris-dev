0c8a486ab89c5364513c0c23a6f126a7c9368964
Jan de Muijnck-Hughes <jfdm@st-andrews.ac.uk>
2016-07-27 22:16:23 +0100

    Bumped Idris to LTS 6.9.

--- stack-shell.nix
+++ stack-shell.nix
@@ -2,7 +2,7 @@ with (import <nixpkgs> {});
 
 let
   # MUST match resolver in stack.yaml
-  resolver = haskell.packages.lts-6_4.ghc;
+  resolver = haskell.packages.lts-6_9.ghc;
 
   native_libs = [
     libffi
