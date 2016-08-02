cfab5ec467ecd6df08c267571fb2618e79bed09b
Steven Shaw <steven@steshaw.org>
2016-08-02 16:07:30 +1000

    Add layout001 to tasty test suite.

--- test/TestData.hs
+++ test/TestData.hs
@@ -156,6 +156,8 @@ testFamiliesData = [
     [ (  1, C_CG ),
       (  2, ANY  ),
       (  3, C_CG )]),
+  ("layout",          "Layout",
+    [ (  1, ANY  )]),
   ("literate",        "Literate programming",
     [ (  1, ANY  )]),
   ("meta",            "Meta-programming",
@@ -297,4 +299,3 @@ testFamiliesData = [
     [ (  1, ANY  ),
       (  2, ANY  ),
       (  3, C_CG )])]
-
