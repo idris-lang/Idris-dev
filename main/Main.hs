250241c5f35bc943e4b2975b661596e8f7d75ad8
Steven Shaw <steven@steshaw.org>
2016-09-08 02:42:45 +1000

    Separate processing of `show`, `client` and `package` options.
    - Made it clearer that execution continues to `idrisMain` if no other options are triggered.

--- main/Main.hs
+++ main/Main.hs
@@ -13,48 +13,69 @@ import Idris.Info.Show
 import Idris.Package
 import Util.System (setupBundledCC)
 
--- Main program reads command line options, parses the main program, and gets
--- on with the REPL.
+processShowOptions :: [Opt] -> Idris ()
+processShowOptions opts = runIO $ do
+  when (ShowAll `elem` opts)          $ showExitIdrisInfo
+  when (ShowLoggingCats `elem` opts)  $ showExitIdrisLoggingCategories
+  when (ShowIncs `elem` opts)         $ showExitIdrisFlagsInc
+  when (ShowLibs `elem` opts)         $ showExitIdrisFlagsLibs
+  when (ShowLibdir `elem` opts)       $ showExitIdrisLibDir
+  when (ShowPkgs `elem` opts)         $ showExitIdrisInstalledPackages
 
-main :: IO ()
-main = do
-  opts <- runArgParser
-  runMain (runIdris opts)
-
-runIdris :: [Opt] -> Idris ()
-runIdris opts = do
-  runIO setupBundledCC
-  when (ShowAll `elem` opts)          $ runIO showExitIdrisInfo
-  when (ShowLoggingCats `elem` opts)  $ runIO showExitIdrisLoggingCategories
-  when (ShowIncs `elem` opts)         $ runIO showExitIdrisFlagsInc
-  when (ShowLibs `elem` opts)         $ runIO showExitIdrisFlagsLibs
-  when (ShowLibdir `elem` opts)       $ runIO showExitIdrisLibDir
-  when (ShowPkgs `elem` opts)         $ runIO showExitIdrisInstalledPackages
+processClientOptions :: [Opt] -> Idris ()
+processClientOptions opts =
   case opt getClient opts of
      []    -> return ()
      (c:_) -> do setVerbose False
                  setQuiet True
-                 runIO $ runClient (getPort opts) c
-                 runIO exitSuccess
+                 runIO $ do
+                   runClient (getPort opts) c
+                   exitSuccess
+
+processPackageOptions :: [Opt] -> Idris ()
+processPackageOptions opts = do
   case opt getPkgCheck opts of
      [] -> return ()
-     fs -> do runIO $ mapM_ (checkPkg opts (WarnOnly `elem` opts) True) fs
-              runIO exitSuccess
+     fs -> runIO $ do
+             mapM_ (checkPkg opts (WarnOnly `elem` opts) True) fs
+             exitSuccess
   case opt getPkgClean opts of
      [] -> return ()
-     fs -> do runIO $ mapM_ (cleanPkg opts) fs
-              runIO exitSuccess
-  case opt getPkgMkDoc opts of                -- IdrisDoc
+     fs -> runIO $ do
+             mapM_ (cleanPkg opts) fs
+             exitSuccess
+  case opt getPkgMkDoc opts of
      [] -> return ()
-     fs -> do runIO $ mapM_ (documentPkg opts) fs
-              runIO exitSuccess
+     fs -> runIO $ do
+             mapM_ (documentPkg opts) fs
+             exitSuccess
   case opt getPkgTest opts of
      [] -> return ()
-     fs -> do runIO $ mapM_ (testPkg opts) fs
-              runIO exitSuccess
+     fs -> runIO $ do
+             mapM_ (testPkg opts) fs
+             exitSuccess
   case opt getPkg opts of
-     [] -> case opt getPkgREPL opts of
-                [] -> idrisMain opts
-                [f] -> replPkg opts f
-                _ -> ifail "Too many packages"
-     fs -> runIO $ mapM_ (buildPkg opts (WarnOnly `elem` opts)) fs
+     [] -> return ()
+     fs -> runIO $ do
+             mapM_ (buildPkg opts (WarnOnly `elem` opts)) fs
+             exitSuccess
+  case opt getPkgREPL opts of
+    []  -> return ()
+    [f] -> do replPkg opts f
+              runIO exitSuccess
+    _   -> ifail "Too many packages"
+
+runIdris :: [Opt] -> Idris ()
+runIdris opts = do
+  runIO setupBundledCC
+  processShowOptions opts
+  processClientOptions opts
+  processPackageOptions opts
+  idrisMain opts
+
+-- Main program reads command line options, parses the main program, and gets
+-- on with the REPL.
+main :: IO ()
+main = do
+  opts <- runArgParser
+  runMain (runIdris opts)
