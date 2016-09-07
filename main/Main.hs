8e99de1b286c685199b5111ab8210c806eb5d62a
Steven Shaw <steven@steshaw.org>
2016-09-08 01:37:43 +1000

    Make imports in `Main` explicit.

--- main/Main.hs
+++ main/Main.hs
@@ -1,64 +1,60 @@
 module Main where
 
-import System.Exit ( exitSuccess )
+import Control.Monad (when)
+import System.Exit (exitSuccess)
 
-import Control.Monad ( when )
-
-import Idris.AbsSyntax
-import Idris.REPL
+import Idris.AbsSyntax (runIO, Idris, setQuiet, setVerbose, Opt(..))
+import Idris.REPL (idrisMain, runMain, runClient)
 import Idris.Error
-import Idris.CmdOptions
+import Idris.CmdOptions (opt, runArgParser, getClient, getPort,
+                         getPkg, getPkgCheck, getPkgClean, getPkgMkDoc, getPkgTest, getPkgREPL)
 import Idris.Info
 import Idris.Info.Show
 import Idris.Package
-
-
-
-import Util.System ( setupBundledCC )
-
-
+import Util.System (setupBundledCC)
 
 -- Main program reads command line options, parses the main program, and gets
 -- on with the REPL.
 
 main :: IO ()
-main = do opts <- runArgParser
-          runMain (runIdris opts)
+main = do
+  opts <- runArgParser
+  runMain (runIdris opts)
 
 runIdris :: [Opt] -> Idris ()
 runIdris opts = do
-    runIO setupBundledCC
-    when (ShowAll `elem` opts)          $ runIO showExitIdrisInfo
-    when (ShowLoggingCats `elem` opts)  $ runIO showExitIdrisLoggingCategories
-    when (ShowIncs `elem` opts)         $ runIO showExitIdrisFlagsInc
-    when (ShowLibs `elem` opts)         $ runIO showExitIdrisFlagsLibs
-    when (ShowLibdir `elem` opts)       $ runIO showExitIdrisLibDir
-    when (ShowPkgs `elem` opts)         $ runIO showExitIdrisInstalledPackages
-    case opt getClient opts of
-       []    -> return ()
-       (c:_) -> do setVerbose False
-                   setQuiet True
-                   runIO $ runClient (getPort opts) c
-                   runIO exitSuccess
-    case opt getPkgCheck opts of
-       [] -> return ()
-       fs -> do runIO $ mapM_ (checkPkg opts (WarnOnly `elem` opts) True) fs
-                runIO exitSuccess
-    case opt getPkgClean opts of
-       [] -> return ()
-       fs -> do runIO $ mapM_ (cleanPkg opts) fs
-                runIO exitSuccess
-    case opt getPkgMkDoc opts of                -- IdrisDoc
-       [] -> return ()
-       fs -> do runIO $ mapM_ (documentPkg opts) fs
-                runIO exitSuccess
-    case opt getPkgTest opts of
-       [] -> return ()
-       fs -> do runIO $ mapM_ (testPkg opts) fs
-                runIO exitSuccess
-    case opt getPkg opts of
-       [] -> case opt getPkgREPL opts of
-                  [] -> idrisMain opts
-                  [f] -> replPkg opts f
-                  _ -> ifail "Too many packages"
-       fs -> runIO $ mapM_ (buildPkg opts (WarnOnly `elem` opts)) fs
+  runIO setupBundledCC
+  when (ShowAll `elem` opts)          $ runIO showExitIdrisInfo
+  when (ShowLoggingCats `elem` opts)  $ runIO showExitIdrisLoggingCategories
+  when (ShowIncs `elem` opts)         $ runIO showExitIdrisFlagsInc
+  when (ShowLibs `elem` opts)         $ runIO showExitIdrisFlagsLibs
+  when (ShowLibdir `elem` opts)       $ runIO showExitIdrisLibDir
+  when (ShowPkgs `elem` opts)         $ runIO showExitIdrisInstalledPackages
+  case opt getClient opts of
+     []    -> return ()
+     (c:_) -> do setVerbose False
+                 setQuiet True
+                 runIO $ runClient (getPort opts) c
+                 runIO exitSuccess
+  case opt getPkgCheck opts of
+     [] -> return ()
+     fs -> do runIO $ mapM_ (checkPkg opts (WarnOnly `elem` opts) True) fs
+              runIO exitSuccess
+  case opt getPkgClean opts of
+     [] -> return ()
+     fs -> do runIO $ mapM_ (cleanPkg opts) fs
+              runIO exitSuccess
+  case opt getPkgMkDoc opts of                -- IdrisDoc
+     [] -> return ()
+     fs -> do runIO $ mapM_ (documentPkg opts) fs
+              runIO exitSuccess
+  case opt getPkgTest opts of
+     [] -> return ()
+     fs -> do runIO $ mapM_ (testPkg opts) fs
+              runIO exitSuccess
+  case opt getPkg opts of
+     [] -> case opt getPkgREPL opts of
+                [] -> idrisMain opts
+                [f] -> replPkg opts f
+                _ -> ifail "Too many packages"
+     fs -> runIO $ mapM_ (buildPkg opts (WarnOnly `elem` opts)) fs
