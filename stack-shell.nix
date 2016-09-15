397f5b89f06016b1df4eecfd467f227b9568b20a
Jan de Muijnck-Hughes <jfdm@st-andrews.ac.uk>
2016-09-15 12:29:44 +0100

    Pushed Stack configuration to latest LTS Haskell Major.

--- stack-shell.nix
+++ stack-shell.nix
@@ -2,7 +2,7 @@ with (import <nixpkgs> {});
 
 let
   # MUST match resolver in stack.yaml
-  resolver = haskell.packages.lts-6_17.ghc;
+  resolver = haskell.packages.lts-7_0.ghc;
 
   native_libs = [
     libffi
