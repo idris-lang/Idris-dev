1f5043030b755e0e75e3ade46c7074df9b013497
Rui Barreiro <rui.barreiro@gmail.com>
2017-06-11 11:45:51 +0100

    added require for the %lib anotations in js backend

--- src/IRTS/JavaScript/Codegen.hs
+++ src/IRTS/JavaScript/Codegen.hs
@@ -71,6 +71,15 @@ getIncludes l = do
   incs <- mapM getInclude l
   return $ T.intercalate "\n\n" incs
 
+includeLibs :: [String] -> String
+includeLibs =
+  let
+    repl '\\' = '_'
+    repl '/' = '_'
+    repl c   = c
+  in
+    concatMap (\lib -> "var " ++ (repl <$> lib) ++ " = require(\"" ++ lib ++"\");\n")
+
 isYes :: Maybe String -> Bool
 isYes (Just "Y") = True
 isYes (Just "y") = True
@@ -105,6 +114,7 @@ codegenJs conf ci =
     extraRT <- TIO.readFile $ path </> (extraRunTime conf)
 
     includes <- getIncludes $ includes ci
+    let libs = T.pack $ includeLibs $ compileLibs ci
     TIO.writeFile (outputFile ci) $ T.concat [ header conf
                                              , "\"use strict\";\n\n"
                                              , "(function(){\n\n"
@@ -114,6 +124,7 @@ codegenJs conf ci =
                                              , doPartials (partialApplications stats), "\n"
                                              , doHiddenClasses (hiddenClasses stats), "\n"
                                              , includes, "\n"
+                                             , libs, "\n"
                                              , out, "\n"
                                              , jsName (sMN 0 "runMain"), "();\n"
                                              , "}.call(this))"
