416251f32374914fb12e2bcac8607317b037fb71
mandubian <pascal@projectseptember.com>
2017-07-22 22:40:20 +0200

    Fixes #1211 : checking default impl in interfaces through parent constraints recursively
    Updated checkDefaultSuperInterfaceImplementation to check default implementations nested in interfaces with respect to constraints of containing interface and if not found, recursively search through constraints of parent interfaces.

--- src/Idris/Elab/Interface.hs
+++ src/Idris/Elab/Interface.hs
@@ -270,11 +270,20 @@ elabInterface info_in syn_in doc what fc constraints tn tnfc ps pDocs fds ds mcn
         = do when (not $ null cs) . tclift
                 $ tfail (At fc (Msg "Default super interface implementations can't have constraints."))
              i <- getIState
-             let t = PApp fc (PRef fc [] n) (map pexp ps)
-             let isConstrained = any (== t) (map snd constraints)
+             let isConstrained = any (checkConstrained i n (map pexp ps)) (map snd constraints)                
              when (not isConstrained) . tclift
                 $ tfail (At fc (Msg "Default implementations must be for a super interface constraint on the containing interface."))
              return ()
+            where 
+                -- First, Check that interface "constraint" has same name & args as default implementation name & args (not checking FC)
+                -- If not the case, recursively lookup in context for parent interface & recheck the same with parent constraints
+                checkConstrained i n args constraint =
+                    if (n == cn && args == cargs)
+                        then True
+                        else any (id) (map (checkConstrained i n args) parentConstraints)
+                    where
+                        PApp _ (PRef _ _ cn) cargs = constraint
+                        parentConstraints = concatMap interface_constraints (lookupCtxt cn (idris_interfaces i))
 
     checkConstraintName :: [Name] -> Name -> Idris ()
     checkConstraintName bound cname
