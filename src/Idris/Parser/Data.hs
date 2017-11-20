bfe345be04185a1434c847893987401da60a3dd2
Jason Felice <jason.m.felice@gmail.com>
2017-11-20 11:54:45 -0500

    Use extents for data declarations

--- src/Idris/Parser/Data.hs
+++ src/Idris/Parser/Data.hs
@@ -179,24 +179,23 @@ data_ syn = (checkDeclFixity $
                         argDocs' = [ (n, annotCode (tryFullExpr syn ist) d)
                                    | (n, d) <- argDocs ]
                     return (doc', argDocs', acc, dataOpts))
-               fc <- getFC
                (tyn_in, nfc) <- withExtent fnName
                (do P.try (lchar ':')
                    ty <- typeExpr (allowImp syn)
                    let tyn = expandNS syn tyn_in
-                   d <- P.option (PData doc argDocs syn fc dataOpts (PLaterdecl tyn nfc ty)) (do
+                   d <- P.option (PData doc argDocs syn nfc dataOpts (PLaterdecl tyn nfc ty)) (do
                      keyword "where"
                      cons <- indentedBlock (constructor syn)
                      accData acc tyn (map (\ (_, _, n, _, _, _, _) -> n) cons)
-                     return $ PData doc argDocs syn fc dataOpts (PDatadecl tyn nfc ty cons))
+                     return $ PData doc argDocs syn nfc dataOpts (PDatadecl tyn nfc ty cons))
                    terminator
                    return d) <|> (do
                     args <- many (do notEndApp
                                      x <- name
                                      return x)
-                    let ty = bindArgs (map (const (PType fc)) args) (PType fc)
+                    let ty = bindArgs (map (const (PType nfc)) args) (PType nfc)
                     let tyn = expandNS syn tyn_in
-                    d <- P.option (PData doc argDocs syn fc dataOpts (PLaterdecl tyn nfc ty)) (do
+                    d <- P.option (PData doc argDocs syn nfc dataOpts (PLaterdecl tyn nfc ty)) (do
                       P.try (lchar '=') <|> do keyword "where"
                                                let kw = (if DefaultEliminator `elem` dataOpts then "%elim" else "") ++ (if Codata `elem` dataOpts then "co" else "") ++ "data "
                                                let n  = show tyn_in ++ " "
@@ -209,12 +208,12 @@ data_ syn = (checkDeclFixity $
                                                let fix3 = s ++ ": " ++ ss ++ " -> Type where\n  ..."
                                                fail $ fixErrorMsg "unexpected \"where\"" [fix1, fix2, fix3]
                       cons <- P.sepBy1 (simpleConstructor (syn { withAppAllowed = False })) (reservedOp "|")
-                      let conty = mkPApp fc (PRef fc [] tyn) (map (PRef fc []) args)
+                      let conty = mkPApp nfc (PRef nfc [] tyn) (map (PRef nfc []) args)
                       cons' <- mapM (\ (doc, argDocs, x, xfc, cargs, cfc, fs) ->
                                    do let cty = bindArgs cargs conty
                                       return (doc, argDocs, x, xfc, cty, cfc, fs)) cons
                       accData acc tyn (map (\ (_, _, n, _, _, _, _) -> n) cons')
-                      return $ PData doc argDocs syn fc dataOpts (PDatadecl tyn nfc ty cons'))
+                      return $ PData doc argDocs syn nfc dataOpts (PDatadecl tyn nfc ty cons'))
                     terminator
                     return d))
            <?> "data type declaration"
