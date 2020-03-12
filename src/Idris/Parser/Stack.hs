d7a669bf95303aafdcd3d9f7df7a7d67658f33de
iko <ilyakooo0@gmail.com>
2020-03-12 22:04:17 +0300

    Migrated to lts-15.3

--- src/Idris/Parser/Stack.hs
+++ src/Idris/Parser/Stack.hs
@@ -77,11 +77,12 @@ parseErrorOffset = P.errorOffset . parseError
 instance Message ParseError where
   messageExtent err = sourcePositionFC pos
     where
-      (pos, _) = P.reachOffsetNoLine (parseErrorOffset err) (parseErrorPosState err)
+      P.PosState {P.pstateSourcePos = pos} =
+        P.reachOffsetNoLine (parseErrorOffset err) (parseErrorPosState err)
   messageText = PP.text . init . P.parseErrorTextPretty . parseError
   messageSource err = Just sline
     where
-      (_, sline, _) = P.reachOffset (parseErrorOffset err) (parseErrorPosState err)
+      (sline, _) = P.reachOffset (parseErrorOffset err) (parseErrorPosState err)
 
 -- | A fully formatted parse error, with caret and bar, etc.
 prettyError :: ParseError -> String
@@ -89,7 +90,7 @@ prettyError =  P.errorBundlePretty . unParseError
 
 {- * Mark and restore -}
 
-type Mark = P.State String
+type Mark = P.State String Void
 
 -- | Retrieve the parser state so we can restart from this point later.
 mark :: Parsing m => m Mark
