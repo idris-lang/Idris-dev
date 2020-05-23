85991c99665aa534791be8e4c954cd5eddbad131
Matus Tejiscak <ziman@functor.sk>
2020-05-23 18:18:09 +0200

    Bump IBC version.

--- src/Idris/IBC.hs
+++ src/Idris/IBC.hs
@@ -41,7 +41,7 @@ import System.Directory
 import System.FilePath
 
 ibcVersion :: Word16
-ibcVersion = 166
+ibcVersion = 167
 
 -- | When IBC is being loaded - we'll load different things (and omit
 -- different structures/definitions) depending on which phase we're in.
